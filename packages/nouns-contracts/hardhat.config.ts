06e127f5769cb9faa7445196852d17f77fd858c6
Dom Hofmann <sevenflow@gmail.com>
2021-06-04 15:46:10 -0400

    Add deploy script

--- packages/nouns-contracts/hardhat.config.ts
+++ packages/nouns-contracts/hardhat.config.ts
@@ -2,6 +2,11 @@ import { HardhatUserConfig, task } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-typechain';
 
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('hardhat-abi-exporter');
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
@@ -12,9 +17,26 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
   }
 });
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: {
     version: '0.7.4',
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`${process.env.WALLET_PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`${process.env.WALLET_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  abiExporter: {
+    path: './abi',
+    clear: true
   },
 };
 export default config;
