fab9c48ebe1d4554b729863a960f3677628143c7
punk9999 <punk9999@protonmail.com>
2021-06-14 14:42:42 -0400

    ERC721Governance: removed ERC-20 code; hook into ERC721.transfer to move delegates; mocked in delegator and token voting lookup; compile works

--- packages/nouns-contracts/contracts/governance/ERC721Governance.sol
+++ packages/nouns-contracts/contracts/governance/ERC721Governance.sol
@@ -1,24 +1,10 @@
-pragma solidity ^0.5.16;
-pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: Unlicense
 
-contract ERC721Governance {
-    /// @notice EIP-20 token name for this token
-    string public constant name = "Token";
+pragma solidity ^0.8.4;
 
-    /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "TOKEN";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 
-    /// @notice EIP-20 token decimals for this token
-    uint8 public constant decimals = 18;
-
-    /// @notice Total number of tokens in circulation
-    uint public constant totalSupply = 10000000e18; // 10 million Token
-
-    /// @notice Allowance amounts on behalf of others
-    mapping (address => mapping (address => uint96)) internal allowances;
-
-    /// @notice Official record of token balances for each account
-    mapping (address => uint96) internal balances;
+abstract contract ERC721Governance is ERC721Enumerable {
 
     /// @notice A record of each accounts delegate
     mapping (address => address) public delegates;
@@ -50,95 +36,29 @@ contract ERC721Governance {
     /// @notice An event thats emitted when a delegate account's vote balance changes
     event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
 
-    /// @notice The standard EIP-20 transfer event
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-
-    /// @notice The standard EIP-20 approval event
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
-
     /**
-     * @notice Construct a new Token token
-     * @param account The initial account to grant all the tokens
-     */
-    constructor(address account) public {
-        balances[account] = uint96(totalSupply);
-        emit Transfer(address(0), account, totalSupply);
+     * @notice The voting power an address can delegate
+    */
+    function delegatorVotes(address delegator) public view returns (uint96) {
+        return safe96(balanceOf(delegator), "ERC721Governance::delegatorVotes: amount exceeds 96 bits");
     }
 
     /**
-     * @notice Get the number of tokens `spender` is approved to spend on behalf of `account`
-     * @param account The address of the account holding the funds
-     * @param spender The address of the account spending the funds
-     * @return The number of tokens approved
-     */
-    function allowance(address account, address spender) external view returns (uint) {
-        return allowances[account][spender];
+     * @notice The voting power of a single tokenId
+    */
+    function tokenVotes(uint256 tokenId) public view returns (uint96) {
+        return 1;
     }
 
     /**
-     * @notice Approve `spender` to transfer up to `amount` from `src`
-     * @dev This will overwrite the approval amount for `spender`
-     *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
-     * @param spender The address of the account which may transfer tokens
-     * @param rawAmount The number of tokens that are approved (2^256-1 means infinite)
-     * @return Whether or not the approval succeeded
-     */
-    function approve(address spender, uint rawAmount) external returns (bool) {
-        uint96 amount;
-        if (rawAmount == uint(-1)) {
-            amount = uint96(-1);
-        } else {
-            amount = safe96(rawAmount, "Token::approve: amount exceeds 96 bits");
-        }
+     * @notice Hooks into `ERC721._transfer` to update delegate votes
+    */
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override {
+        super._beforeTokenTransfer(from, to, tokenId);
 
-        allowances[msg.sender][spender] = amount;
+        uint96 amount = tokenVotes(tokenId);
 
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    /**
-     * @notice Get the number of tokens held by the `account`
-     * @param account The address of the account to get the balance of
-     * @return The number of tokens held
-     */
-    function balanceOf(address account) external view returns (uint) {
-        return balances[account];
-    }
-
-    /**
-     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
-     * @param dst The address of the destination account
-     * @param rawAmount The number of tokens to transfer
-     * @return Whether or not the transfer succeeded
-     */
-    function transfer(address dst, uint rawAmount) external returns (bool) {
-        uint96 amount = safe96(rawAmount, "Token::transfer: amount exceeds 96 bits");
-        _transferTokens(msg.sender, dst, amount);
-        return true;
-    }
-
-    /**
-     * @notice Transfer `amount` tokens from `src` to `dst`
-     * @param src The address of the source account
-     * @param dst The address of the destination account
-     * @param rawAmount The number of tokens to transfer
-     * @return Whether or not the transfer succeeded
-     */
-    function transferFrom(address src, address dst, uint rawAmount) external returns (bool) {
-        address spender = msg.sender;
-        uint96 spenderAllowance = allowances[src][spender];
-        uint96 amount = safe96(rawAmount, "Token::approve: amount exceeds 96 bits");
-
-        if (spender != src && spenderAllowance != uint96(-1)) {
-            uint96 newAllowance = sub96(spenderAllowance, amount, "Token::transferFrom: transfer amount exceeds spender allowance");
-            allowances[src][spender] = newAllowance;
-
-            emit Approval(src, spender, newAllowance);
-        }
-
-        _transferTokens(src, dst, amount);
-        return true;
+        _moveDelegates(from, to, amount);
     }
 
     /**
@@ -159,13 +79,14 @@ contract ERC721Governance {
      * @param s Half of the ECDSA signature pair
      */
     function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s) public {
-        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), getChainId(), address(this)));
+
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name())), getChainId(), address(this)));
         bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "Token::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "Token::delegateBySig: invalid nonce");
-        require(now <= expiry, "Token::delegateBySig: signature expired");
+        require(signatory != address(0), "ERC721Governance::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "ERC721Governance::delegateBySig: invalid nonce");
+        require(block.timestamp <= expiry, "ERC721Governance::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -187,7 +108,7 @@ contract ERC721Governance {
      * @return The number of votes the account had as of the given block
      */
     function getPriorVotes(address account, uint blockNumber) public view returns (uint96) {
-        require(blockNumber < block.number, "Token::getPriorVotes: not yet determined");
+        require(blockNumber < block.number, "ERC721Governance::getPriorVotes: not yet determined");
 
         uint32 nCheckpoints = numCheckpoints[account];
         if (nCheckpoints == 0) {
@@ -222,23 +143,13 @@ contract ERC721Governance {
 
     function _delegate(address delegator, address delegatee) internal {
         address currentDelegate = delegates[delegator];
-        uint96 delegatorBalance = balances[delegator];
         delegates[delegator] = delegatee;
 
         emit DelegateChanged(delegator, currentDelegate, delegatee);
 
-        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
-    }
+        uint96 amount = delegatorVotes(delegator);
 
-    function _transferTokens(address src, address dst, uint96 amount) internal {
-        require(src != address(0), "Token::_transferTokens: cannot transfer from the zero address");
-        require(dst != address(0), "Token::_transferTokens: cannot transfer to the zero address");
-
-        balances[src] = sub96(balances[src], amount, "Token::_transferTokens: transfer amount exceeds balance");
-        balances[dst] = add96(balances[dst], amount, "Token::_transferTokens: transfer amount overflows");
-        emit Transfer(src, dst, amount);
-
-        _moveDelegates(delegates[src], delegates[dst], amount);
+        _moveDelegates(currentDelegate, delegatee, amount);
     }
 
     function _moveDelegates(address srcRep, address dstRep, uint96 amount) internal {
@@ -246,21 +157,21 @@ contract ERC721Governance {
             if (srcRep != address(0)) {
                 uint32 srcRepNum = numCheckpoints[srcRep];
                 uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint96 srcRepNew = sub96(srcRepOld, amount, "Token::_moveVotes: vote amount underflows");
+                uint96 srcRepNew = sub96(srcRepOld, amount, "ERC721Governance::_moveDelegates: amount underflows");
                 _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
             }
 
             if (dstRep != address(0)) {
                 uint32 dstRepNum = numCheckpoints[dstRep];
                 uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint96 dstRepNew = add96(dstRepOld, amount, "Token::_moveVotes: vote amount overflows");
+                uint96 dstRepNew = add96(dstRepOld, amount, "ERC721Governance::_moveDelegates: amount overflows");
                 _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
             }
         }
     }
 
     function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
-      uint32 blockNumber = safe32(block.number, "Token::_writeCheckpoint: block number exceeds 32 bits");
+      uint32 blockNumber = safe32(block.number, "ERC721Governance::_writeCheckpoint: block number exceeds 32 bits");
 
       if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
           checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
@@ -293,7 +204,7 @@ contract ERC721Governance {
         return a - b;
     }
 
-    function getChainId() internal pure returns (uint) {
+    function getChainId() internal view returns (uint) {
         uint256 chainId;
         assembly { chainId := chainid() }
         return chainId;
