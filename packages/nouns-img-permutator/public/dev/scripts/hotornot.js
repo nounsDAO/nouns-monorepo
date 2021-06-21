16be7fb2909b79291a1890b72b9266da778156f6
cryptoseneca <sovincrypto@gmail.com>
2021-06-21 16:16:07 -0400

    simplify hot-or-not UX and UI.

--- packages/nouns-img-permutator/public/dev/scripts/hotornot.js
+++ packages/nouns-img-permutator/public/dev/scripts/hotornot.js
@@ -1,19 +1,30 @@
 
+const randomNounURL = 'https://us-central1-nounsdao.cloudfunctions.net/generateNounUsingOptionsAndSourceAndBG'
+const voteURL = 'https://us-central1-nounsdao.cloudfunctions.net/addVoteToLayersINT'
+
+// holds nouns image data
+var randomNounsData = []
+var voteStreak = 0
+ 
 
 window.onload = async () => {
+    
     await fetchRandomNouns()
+    
+    // remove init loading title
+    document.getElementById('loading-title').remove()
+    
+    // show vote streak
+    updateVoteStreak()
+
+    // display noun colums
+    showNounColums()
 }
 
-const randomNounURL = 'https://us-central1-nounsdao.cloudfunctions.net/generateNounUsingOptionsAndSource'
-const voteURL = 'https://us-central1-nounsdao.cloudfunctions.net/addVoteToLayersEXT'
-var randomNounsData = []
- 
 fetchRandomNouns = async () => {
 
-    // prep UI for loading
-    showNounColum(false, 0)
-    showNounColum(false, 1)
-    showLoadingTitle()
+    // show loading
+    displayNounsLoading(true)
 
     // set up options for random 
     let options = {
@@ -29,101 +40,93 @@ fetchRandomNouns = async () => {
     for (var i = 0; i < 2; i++) {
         let res = await fetch(`${randomNounURL}?options=${JSON.stringify(options)}`)
         let json = await res.json()
+        console.log(json)
         randomNounsData.push(json)        
     }
     
     // reset ui
-    showNounColum(true, 0)
-    showNounColum(true, 1)
-    hideLoadingTitle()
+    displayNounsLoading(false)
 
     // set new noun imgs
-    document.getElementById('noun-0-img').setAttribute('src', randomNounsData[0].base64)
-    document.getElementById('noun-1-img').setAttribute('src', randomNounsData[1].base64)
+    for (var i = 0; i < 2; i++) {
+        let img = document.getElementById(`noun-${i}-img`)
+        img.setAttribute('src', randomNounsData[i].base64)
+        let hsl = `hsl(${randomNounsData[i].dominantColorHSL[0]},${randomNounsData[i].dominantColorHSL[1]-10}%,${70}%)`
+        img.style.backgroundColor = hsl
+    }
 }
 /**
- * EXPLAIN
+ * Sends vote to backend, updates vote streak and initiates new noun fetch.
  * @param {Number} selectionIndex Index of selected noun (i.e. left = 0, right = 1)
  */
-voted = async (selectionIndex, button) => {
+voted = async (selectionIndex) => {
 
-    // set voted noun button as disabled/loading
-    setButtonLoading(button, true)
-
-    // hide non voted noun column
-    let nonSelectedNounIndex = selectionIndex == 0 ? 1 : 0;
-    showNounColum(false, nonSelectedNounIndex)
-    
     // send vote to backend and display results
-    let selectedNounData = randomNounsData[selectionIndex]
-    try {
-        let res = await fetch(`${voteURL}?options=${JSON.stringify(selectedNounData.layers)}`)
-        let json = await res.json()        
-        displayResults(json)
-    } catch (e) {
-        console.log(e)
-    }
-    
-    // remove button loading
-    setButtonLoading(button, false, "NEXT")
+    let selectedNoun = randomNounsData[selectionIndex]
+    fetch(`${voteURL}?options=${JSON.stringify(selectedNoun.layers)}`)
 
-    // upate functionality of voted noun button to go to next set of nouns
-    button.onclick = () => {  resetUI(selectionIndex, button) }
-}
+    // update vote
+    voteStreak++;
+    updateVoteStreak()
 
-displayResults = (results) => {
-    
-    // colum to hold results
-    let column = document.createElement('div')
-    column.classList.add('col-lg-6')
-    column.classList.add('d-flex')
-    column.classList.add('flex-column')
-    column.classList.add('justify-content-center')    
-    column.classList.add('align-items-start')
-    column.style.minHeight = '500px'
-    column.id = 'results'
-
-    // results
-    results.updatedDocs.shift() // remove layer-0 or "background layer" from results
-
-    // title
-    let div = document.createElement('div')
-    div.classList.add('fs-1')
-    div.classList.add('lond-font')
-    div.style.color = 'gray'
-    div.textContent = "VOTE TALLY:"
-    column.appendChild(div)
-
-    results.updatedDocs.forEach(doc => {
-        let div = document.createElement('div')
-        div.classList.add('fs-5')
-        div.classList.add('lond-font')
-        div.textContent = doc.fileName + ': ' + doc.votes + ' votes'
-        column.appendChild(div)
-    })        
-
-    document.getElementById('hot-or-not-content').appendChild(column)
-}
-
-resetUI = (index, button) => {
-
-    // reset button text and `onclick`
-    button.textContent = "VOTE"
-    button.onclick = () => { voted(index, button) }
-
-    // remove results div
-    document.getElementById('results').remove()
-
-    // fetch new nouns
+    // start next round of nouns
     fetchRandomNouns()
 }
 
-showNounColum = (show, index) => {
-    if (show) {
-        document.getElementById(`noun-${index}-col`).style.display = 'block'
-    } else {
-        document.getElementById(`noun-${index}-col`).style.display = 'none'
+/**
+ * Sets noun images as 'loading nouns' and accompanying buttons.
+ * @param {Boolean} loading Displays or hides loading signifier on buttons
+ */
+displayNounsLoading = (loading) => {
+
+    // set "non-noun" as noun imgs
+    for (var i = 0; i < 2; i++) {
+        let img = document.getElementById(`noun-${i}-img`)
+        img.setAttribute('src', 'assets/question-noun.png')
+        img.style.backgroundColor = ''
     }
+    
+    // set buttons as loading
+    let buttons = document.getElementsByTagName('button')
+    for (var i = 0; i < buttons.length; i++) {
+        if (loading) {
+            setButtonLoading(buttons[i], true)
+        } else {
+            setButtonLoading(buttons[i], false, 'VOTE')
+        }
+    }
+}
+/**
+ * Updates copy of voting streak header
+ */
+updateVoteStreak = () => {
+    var addOn = ''
+
+    if (voteStreak >= 3 && voteStreak <= 10) {
+        addOn = '🔥'
+    } else if (voteStreak >= 10 && voteStreak <= 25) {
+        addOn = '⚡️'
+    } else if (voteStreak >= 25 && voteStreak <= 50) {
+        addOn = '🚀'
+    } else if (voteStreak >= 50 && voteStreak <= 100) {
+        addOn = '🧨'
+    } else if (voteStreak >= 100) {
+        addOn = '💣'
+    } 
+    
+    let row = document.getElementById('vote-streak-row')
+    row.innerHTML = ''
+    
+    let title = document.createElement('h1')
+    title.classList.add('display-5')
+    title.textContent = `Vote streak: ${voteStreak}${addOn}`     
+    
+    row.appendChild(title)
+}
+
+showNounColums = () => {
+    document.getElementById(`noun-0-col`).style.display = 'block'
+    document.getElementById(`noun-1-col`).style.display = 'block'
 }
 
 showLoadingTitle = () => {
