76da46a44b5cf55230728691c1cf797aa41ac959
solimander <solimander19@gmail.com>
2021-07-05 21:18:16 -0600

    Reduce timeout

--- packages/nouns-contracts/hardhat.config.ts
+++ packages/nouns-contracts/hardhat.config.ts
@@ -50,7 +50,7 @@ const config: HardhatUserConfig = {
     coinmarketcap: '7643dfc7-a58f-46af-8314-2db32bdd18ba',
   },
   mocha: {
-    timeout: 120_000,
+    timeout: 60_000,
   },
 };
 export default config;
