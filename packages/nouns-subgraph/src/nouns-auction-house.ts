548246370c977cc41f4fb74586ee1a520b5124a1
solimander <solimander19@gmail.com>
2021-07-08 17:56:14 -0600

    Subgraph additions

--- packages/nouns-subgraph/src/nouns-auction-house.ts
+++ packages/nouns-subgraph/src/nouns-auction-house.ts
@@ -1,13 +1,85 @@
-import { log, BigInt } from '@graphprotocol/graph-ts';
+import { BigInt, log } from '@graphprotocol/graph-ts';
 import {
+  AuctionBid,
   AuctionCreated,
-} from "./types/NounsAuctionHouse/NounsAuctionHouse"
-import { Auction } from './types/schema';
+  AuctionExtended,
+  AuctionSettled,
+} from './types/NounsAuctionHouse/NounsAuctionHouse';
+import { Account, Auction, Noun } from './types/schema';
 
 export function handleAuctionCreated(event: AuctionCreated): void {
-  let id = event.params.nounId.toString();
-  log.info('[handleAuctionCreated] Add new Auction {} at block {}', [id, event.block.number.toString()]);
-  let nounsAuction = new Auction(id);
-  nounsAuction.nounId = BigInt.fromString(id);
-  nounsAuction.save();
+  let nounId = event.params.nounId.toString();
+
+  let noun = Noun.load(nounId);
+  if (!noun) {
+    log.error('[handleAuctionCreated] Noun #{} not found. Hash: {}', [
+      nounId,
+      event.transaction.hash.toHex(),
+    ]);
+    return;
+  }
+
+  let auction = new Auction(nounId);
+  auction.noun = noun.id;
+  auction.amount = BigInt.fromI32(0);
+  auction.startTime = event.params.startTime;
+  auction.endTime = event.params.endTime;
+  auction.settled = false;
+  auction.save();
+}
+
+export function handleAuctionBid(event: AuctionBid): void {
+  let nounId = event.params.nounId.toString();
+  let bidderAddress = event.params.sender.toHex();
+
+  let bidder = Account.load(bidderAddress);
+  if (!bidder) {
+    bidder = new Account(bidderAddress);
+    bidder.save();
+  }
+
+  let auction = Auction.load(nounId);
+  if (!auction) {
+    log.error('[handleAuctionBid] Auction not found for Noun #{}. Hash: {}', [
+      nounId,
+      event.transaction.hash.toHex(),
+    ]);
+    return;
+  }
+
+  auction.amount = event.params.value;
+  auction.bidder = bidder.id;
+  auction.save();
+}
+
+export function handleAuctionExtended(event: AuctionExtended): void {
+  let nounId = event.params.nounId.toString();
+
+  let auction = Auction.load(nounId);
+  if (!auction) {
+    log.error('[handleAuctionExtended] Auction not found for Noun #{}. Hash: {}', [
+      nounId,
+      event.transaction.hash.toHex(),
+    ]);
+    return;
+  }
+
+  auction.endTime = event.params.endTime;
+  auction.save();
+}
+
+export function handleAuctionSettled(event: AuctionSettled): void {
+  let nounId = event.params.nounId.toString();
+
+  let auction = Auction.load(nounId);
+  if (!auction) {
+    log.error('[handleAuctionSettled] Auction not found for Noun #{}. Hash: {}', [
+      nounId,
+      event.transaction.hash.toHex(),
+    ]);
+    return;
+  }
+
+  auction.settled = true;
+  auction.save();
 }
