d4fbbc60d0bbc5381b51c367c91bc249bc811c5c
cryptoseneca <sovincrypto@gmail.com>
2021-07-08 16:30:08 -0400

    add alt atribute to silence warning

--- packages/nouns-webapp/src/components/Shared/Noun.tsx
+++ packages/nouns-webapp/src/components/Shared/Noun.tsx
@@ -2,7 +2,7 @@ import classes from './Noun.module.css';
 import React from 'react';
 
 const Noun: React.FC<{ imgPath: string }> = props => {
-  return <img className={classes.img} src={props.imgPath} />;
+  return <img className={classes.img} src={props.imgPath} alt="A happy Nouns DAO member." />;
 };
 
 export default Noun;
