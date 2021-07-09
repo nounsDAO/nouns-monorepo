7c0f21aec3f8f8e1a5e98acdf949900ad8f0f35e
cryptoseneca <sovincrypto@gmail.com>
2021-07-09 09:25:26 -0400

    add placing bid functionality to activity lens

--- packages/nouns-webapp/src/components/NounGlasses/ActivityLens.tsx
+++ packages/nouns-webapp/src/components/NounGlasses/ActivityLens.tsx
@@ -1,7 +1,18 @@
 import Lens from './Lens';
 import classes from './ActivityLens.module.css';
+import React from 'react';
+import { useRef } from 'react';
 
-const ActivityLens = () => {
+const ActivityLens: React.FC<{ onPlaceBid: (bidAmount: number) => void }> = props => {
+  const placeBidInputRef = useRef<HTMLInputElement>(null);
+
+  const placeBidHandler = () => {
+    const bid = Number(placeBidInputRef.current!.value);
+    if (bid > 0) {
+      props.onPlaceBid(bid);
+    }
+  };
+
   return (
     <Lens zIndex={2}>
       <div className={classes.activityContainer}>
@@ -13,8 +24,16 @@ const ActivityLens = () => {
           <li key={Math.random()}>0.01 0xabc 1h</li>
         </ul>
       </div>
-      <button className={classes.bidBtn}>BID</button>
-      <input className={classes.bidInput} type="number" placeholder="ETH" min="0"></input>
+      <button className={classes.bidBtn} onClick={placeBidHandler}>
+        BID
+      </button>
+      <input
+        className={classes.bidInput}
+        ref={placeBidInputRef}
+        type="number"
+        placeholder="ETH"
+        min="0"
+      ></input>
     </Lens>
   );
 };
--- packages/nouns-webapp/src/components/NounGlasses/NounGlasses.tsx
+++ packages/nouns-webapp/src/components/NounGlasses/NounGlasses.tsx
@@ -5,20 +5,35 @@ import CenterHandle from './CenterHandle';
 import ActivityLens from './ActivityLens';
 
 import { useNounToken } from '../../wrappers/nounToken';
-import { useAuction } from '../../wrappers/nounsAuction';
-import { BigNumber } from '@usedapp/core/node_modules/ethers';
+import {
+  useAuction,
+  auctionHouseContractFactory,
+  AuctionHouseContractFunctions,
+} from '../../wrappers/nounsAuction';
+import { useContractFunction } from '@usedapp/core';
+import { BigNumber, utils } from '@usedapp/core/node_modules/ethers';
 import config from '../../config';
 
 const NounGlasses = () => {
   const noun = useNounToken(BigNumber.from('33'));
   const auction = useAuction(config.auctionProxyAddress);
 
+  const auctionHouseContract = auctionHouseContractFactory(config.auctionProxyAddress);
+  const { state, send } = useContractFunction(
+    auctionHouseContract as any,
+    AuctionHouseContractFunctions.createBid,
+  );
+
+  const placeBid = (bidAmount: number) => {
+    send(auction.nounId, { value: utils.parseEther(bidAmount.toString()) });
+  };
+
   return (
     <Row noGutters={true}>
       <LeftHandle nounId={auction ? auction.nounId.toString() : ''} />
       <NounsLens imgSrc={noun ? noun.image : ''} />
       <CenterHandle />
-      <ActivityLens />
+      <ActivityLens onPlaceBid={placeBid} />
     </Row>
   );
 };
