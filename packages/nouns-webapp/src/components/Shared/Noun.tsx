468bcb6184c03ba0c5e62800e372d3d5347cd45d
cryptoseneca <sovincrypto@gmail.com>
2021-07-10 13:21:22 -0400

    display 'question-noun' when Noun component is passed empty string

--- packages/nouns-webapp/src/components/Shared/Noun.tsx
+++ packages/nouns-webapp/src/components/Shared/Noun.tsx
@@ -1,8 +1,15 @@
 import classes from './Noun.module.css';
 import React from 'react';
+import questionNoun from '../../assets/question-noun.png';
 
 const Noun: React.FC<{ imgPath: string }> = props => {
-  return <img className={classes.img} src={props.imgPath} alt="A happy Nouns DAO member." />;
+  return (
+    <img
+      className={classes.img}
+      src={props.imgPath ? props.imgPath : questionNoun}
+      alt="A happy Nouns DAO member."
+    />
+  );
 };
 
 export default Noun;
