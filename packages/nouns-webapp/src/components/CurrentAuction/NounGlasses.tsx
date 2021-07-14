6406e0b4e9a04913774eba8b81085066b0265741
cryptoseneca <sovincrypto@gmail.com>
2021-07-14 11:51:13 -0400

    Delete handles from NounGlasses

--- packages/nouns-webapp/src/components/CurrentAuction/NounGlasses.tsx
+++ packages/nouns-webapp/src/components/CurrentAuction/NounGlasses.tsx
@@ -1,22 +1,19 @@
 import { Row } from 'react-bootstrap';
-import LeftHandle from './LeftHandle';
 import NounsLens from './NounsLens';
-import CenterHandle from './CenterHandle';
 import ActivityLens from './ActivityLens';
 import LoadingNounLens from './LoadingNounLens';
 
 import { useAuction } from '../../wrappers/nounsAuction';
 import { BigNumber } from '@usedapp/core/node_modules/ethers';
 import config from '../../config';
+import StandaloneNoun from '../StandaloneNoun';
 
 const NounGlasses = () => {
   const auction = useAuction(config.auctionProxyAddress);
 
   return (
     <Row noGutters={true}>
-      <LeftHandle />
       {auction ? <NounsLens nounId={BigNumber.from(auction.nounId)} /> : <LoadingNounLens />}
-      <CenterHandle />
       <ActivityLens auction={auction && auction} />
     </Row>
   );
