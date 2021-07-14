be1dd36246169744fe1521f71d356786b6ec74c1
solimander <solimander19@gmail.com>
2021-07-13 19:19:22 -0600

    Log accounts

--- packages/nouns-contracts/tasks/run-local.ts
+++ packages/nouns-contracts/tasks/run-local.ts
@@ -26,6 +26,18 @@ task(
 
   const { chainId } = await ethers.provider.getNetwork();
 
+  const accounts = {
+    'Account #0': {
+      Address: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+      'Private Key': '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+    },
+    'Account #1': {
+      Address: '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
+      'Private Key': '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+    },
+  };
+
+  console.table(accounts);
   console.log(
     `Noun contracts deployed to local node at http://localhost:8545 (Chain ID: ${chainId})`,
   );
