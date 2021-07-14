9a3bc723cd8d6dbe705a0d8b81c50e848a4e935c
cryptoseneca <sovincrypto@gmail.com>
2021-07-14 11:10:14 -0400

    Update Noun component to take an alt description prop

--- packages/nouns-webapp/src/components/NounGlasses/NounsLens.tsx
+++ packages/nouns-webapp/src/components/NounGlasses/NounsLens.tsx
@@ -7,7 +7,7 @@ const NounsLens: React.FC<{ nounId: BigNumber }> = props => {
   const noun = useNounToken(props.nounId);
   return (
     <Lens zIndex={4}>
-      <Noun imgPath={noun ? noun.image : ''} />
+      <Noun imgPath={noun ? noun.image : ''} alt={noun ? noun.description : ''} />
     </Lens>
   );
 };
