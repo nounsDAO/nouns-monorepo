999f5d7632e7d5161a218546600c78fe8a9f5eb3
cryptoseneca <sovincrypto@gmail.com>
2021-07-15 15:59:42 -0400

    Update nav bar styling

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -1,55 +1,38 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { Container } from 'react-bootstrap';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Form from 'react-bootstrap/Form';
-// import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import { useAppSelector  } from '../../hooks';
+import { useAppSelector } from '../../hooks';
 import { useEthers } from '@usedapp/core';
+import { Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 import ShortAddress from '../ShortAddress';
+import classes from './NavBar.module.css';
 
 const NavBar = () => {
   const activeAccount = useAppSelector(state => state.account.activeAccount);
   const { activateBrowserWallet } = useEthers();
 
+  const connectedContent = activeAccount ? (
+    <div className={classes.connectedDiv}>
+      <ShortAddress>{activeAccount}</ShortAddress>
+    </div>
+  ) : (
+    <button className={classes.connectButton} onClick={() => activateBrowserWallet()}>
+      Connect Wallet
+    </button>
+  );
+
   return (
     <Container>
-    <Navbar bg="transparent" expand="lg">
-      <Navbar.Brand href="#home">
-        <img
-          src="/logo.svg"
-          width="70"
-          height="70"
-          className="d-inline-block align-middle"
-          alt="React Bootstrap logo"
-        />
-      </Navbar.Brand>
-
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          {/* <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown> */}
-        </Nav>
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
-        {activeAccount ? (
-          <ShortAddress>{activeAccount}</ShortAddress>
-        ) : (
-          <Button onClick={() => activateBrowserWallet()}>Connect Wallet</Button>
-        )}
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar bg="transparent" expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo.svg"
+            width="70"
+            height="70"
+            className="d-inline-block align-middle"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        {connectedContent}
+      </Navbar>
     </Container>
   );
 };
