61b7145a523340f348fbbb66badf9cc556a2bb3a
cryptoseneca <sovincrypto@gmail.com>
2021-07-16 13:28:35 -0400

    Update addresses to v3 testnet contracts

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
-  auctionProxyAddress: '0x6DbDc9346140792B9dd024D01C3c532d12907666',
-  tokenAddress: '0xD6e4cbd35832193b477A968307c99f74F59C9df7',
-  subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby'
+  auctionProxyAddress: '0xBA088c634394775D89cAC7c67DFD52D73bfdFa05',
+  tokenAddress: '0xc52bb4Fc4ed72f2a910BF0481D620B927Ded76f7',
+  subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby',
 };
 
 export default config;
