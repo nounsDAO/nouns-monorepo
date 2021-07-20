6b05316e62330af9df54d05327231a1188d01626
punk4156 <80659633+punk4156@users.noreply.github.com>
2021-07-20 10:47:52 -0400

    test

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -1,11 +1,16 @@
 import classes from './Banner.module.css';
 import Section from '../../layout/Section';
 import { Col } from 'react-bootstrap';
+import panelAnimation from '../../assets/panel-animation.gif'
+import Image from 'react-bootstrap/Image'
 
 const Banner = () => {
   return (
     <Section bgColor="white" fullWidth={false}>
-      <Col lg={{ span: 10, offset: 1 }}>
+      <Col lg={{ span: 6}}>
+      <Image src={panelAnimation} fluid />
+      </Col>
+      <Col lg={{ span: 6}}>
         <div className={classes.wrapper}>
           <h1>ONE NOUN,</h1>
           <h1>EVERY DAY,</h1>
