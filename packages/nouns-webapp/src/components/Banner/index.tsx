c130c1c82d82c229406839ff5ad387d25f8ccce9
cryptoseneca <sovincrypto@gmail.com>
2021-07-21 08:50:16 -0500

    Remove row because Section already contains Row and apply lint

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -1,22 +1,26 @@
 import classes from './Banner.module.css';
 import Section from '../../layout/Section';
-import { Col, Row } from 'react-bootstrap';
-import panelAnimation from '../../assets/panel-animation2.gif'
-import Image from 'react-bootstrap/Image'
+import { Col } from 'react-bootstrap';
+import panelAnimation from '../../assets/panel-animation2.gif';
+import Image from 'react-bootstrap/Image';
 
 const Banner = () => {
   return (
     <Section bgColor="white" fullWidth={false}>
-      <Row className='align-items-center'>
-      <Col lg={{ span: 6}}>
+      <Col lg={6}>
         <div className={classes.wrapper}>
-          <h1>ONE NOUN, EVERY DAY,<br/>FOREVER.</h1>
+          <h1>
+            ONE NOUN,
+            <br />
+            EVERY DAY,
+            <br />
+            FOREVER.
+          </h1>
         </div>
       </Col>
-      <Col lg={{ span: 6}}>
+      <Col lg={6}>
         <Image className={classes.nounGif} src={panelAnimation} fluid />
       </Col>
-      </Row>
     </Section>
   );
 };
