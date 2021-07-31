8c2bce854f5559daf06ab53354d622dc1790a020
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-07-30 20:17:42 -0500

    Replace question loading noun for animation noun gif

--- packages/nouns-webapp/src/components/Noun/index.tsx
+++ packages/nouns-webapp/src/components/Noun/index.tsx
@@ -1,11 +1,11 @@
 import classes from './Noun.module.css';
 import React from 'react';
-import questionNoun from '../../assets/question-noun.png';
+import loadingNoun from '../../assets/loadnoun.gif';
 import Image from 'react-bootstrap/Image';
 
 const Noun: React.FC<{ imgPath: string; alt: string }> = props => {
   const { imgPath, alt } = props;
-  return <Image className={classes.img} src={imgPath ? imgPath : questionNoun} alt={alt} fluid />;
+  return <Image className={classes.img} src={imgPath ? imgPath : loadingNoun} alt={alt} fluid />;
 };
 
 export default Noun;
