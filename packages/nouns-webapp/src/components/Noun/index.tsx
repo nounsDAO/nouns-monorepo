01228a9bd0f584d95fb7c4ec6012dd36d15ab0da
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-08-02 13:46:14 -0500

    Update loading noun gif asset

--- packages/nouns-webapp/src/components/Noun/index.tsx
+++ packages/nouns-webapp/src/components/Noun/index.tsx
@@ -1,6 +1,6 @@
 import classes from './Noun.module.css';
 import React from 'react';
-import loadingNoun from '../../assets/loadnoun.gif';
+import loadingNoun from '../../assets/loading-skull-noun.gif';
 import Image from 'react-bootstrap/Image';
 
 const Noun: React.FC<{ imgPath: string; alt: string }> = props => {
