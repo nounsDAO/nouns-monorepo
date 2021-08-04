6ee4b56d1ed53203de145f3c8e04ec32613511c2
punk9999 <punk9999@protonmail.com>
2021-08-04 14:41:36 -0400

    Revert "refactor auction activity"
    This reverts commit 7ed3f880433bfe53e47799fd5e658b74d6bd0fa1.

--- packages/nouns-webapp/src/components/ShortAddress/index.tsx
+++ packages/nouns-webapp/src/components/ShortAddress/index.tsx
@@ -1,6 +1,6 @@
 const ShortAddress = (props: { children: string | undefined }) => {
   const { children } = props;
-  return <>{children && [children.substr(0, 6), children.substr(37, 7)].join('...')}</>;
+  return <div>{children && [children.substr(0, 6), children.substr(37, 7)].join('...')}</div>;
 };
 
 export default ShortAddress;
