5a8251947d92a6c3fe9d160bba4649c163489062
solimander <solimander19@gmail.com>
2021-08-06 22:25:57 -0600

    Add title to NFT descriptor

--- packages/nouns-contracts/contracts/libs/NFTDescriptor.sol
+++ packages/nouns-contracts/contracts/libs/NFTDescriptor.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
+/// @title A library used to construct ERC721 token URIs and SVG images
+
 /*********************************
  * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
  * ░░░░░░▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒░░░ *
