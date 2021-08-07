8e3abcdd50d2af63fbc1bf124e3c0f33ef134a2e
devcarrot <devcarrot@protonmail.com>
2021-08-06 20:51:12 -0600

    Update to V4 subgraph

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   auctionProxyAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
   tokenAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
-  subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby',
+  subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v4',
 };
 
 export default config;
