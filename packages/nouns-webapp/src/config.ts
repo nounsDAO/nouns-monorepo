886560d53d811f506302ddba31ceccd2d175e6f6
devcarrot <devcarrot@protonmail.com>
2021-08-06 20:52:54 -0600

    Correct token address

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   auctionProxyAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
-  tokenAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
+  tokenAddress: '0x632f34c3aee991b10D4b421Bc05413a03d7a37eB',
   subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v4',
 };
 
