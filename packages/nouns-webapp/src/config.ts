efbae51bb5b1d7fe1b901c5c2ea671d53577196d
devcarrot <devcarrot@protonmail.com>
2021-08-06 21:43:59 -0600

    Re-correct the subgraph url

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -16,7 +16,7 @@ const config: Record<SupportedChains, Config> = {
     auctionProxyAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
     tokenAddress: '0x632f34c3aee991b10D4b421Bc05413a03d7a37eB',
     nounsDaoAddress: '0xd1C753D9A23eb5c57e0d023e993B9bd4F5086b04',
-    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v2',
+    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v4',
   },
   [ChainId.Mainnet]: {
     auctionProxyAddress: '0x0000000000000000000000000000000000000000',
