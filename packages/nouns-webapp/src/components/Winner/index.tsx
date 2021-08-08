e606e66d7fec6d630c9b1a359eae486091488f3d
punk9999 <punk9999@protonmail.com>
2021-08-08 09:33:35 -0400

    Winner displays 'You' if winner is active address. Also displays null address if Noun is burned

--- packages/nouns-webapp/src/components/Winner/index.tsx
+++ packages/nouns-webapp/src/components/Winner/index.tsx
@@ -1,14 +1,17 @@
 import classes from '../CurrentBid/CurrentBid.module.css';
 import ShortAddress from '../ShortAddress';
+import { useAppSelector } from '../../hooks';
 
 const Winner: React.FC<{ winner: string }> = props => {
+  const activeAccount = useAppSelector(state => state.account.activeAccount);
+
   const { winner } = props;
 
   return (
     <div className={classes.section}>
       <h4>Winner</h4>
       <h2>
-        <ShortAddress>{winner}</ShortAddress>
+        {!winner ? '0x0000' : winner === activeAccount?.toLowerCase() ? 'You' : <ShortAddress>{winner}</ShortAddress>}
       </h2>
     </div>
   );
