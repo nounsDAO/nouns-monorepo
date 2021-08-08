813d7f3238f8ca98ed332cc7a8f4e0ad82879f6b
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-08-08 10:25:06 -0500

    Add links to footer

--- packages/nouns-webapp/src/components/Footer/index.tsx
+++ packages/nouns-webapp/src/components/Footer/index.tsx
@@ -1,11 +1,15 @@
 import classes from './Footer.module.css';
 import Section from '../../layout/Section';
-import heartNoun from '../../assets/heart-noun.png';
 import { useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
+import { buildEtherscanAddressLink, Network } from '../../utils/buildEtherscanLink';
+import { externalURL, ExternalURL } from '../../utils/externalURL';
+import config from '../../config';
 
 const Footer = () => {
-  const twitterURL = 'https://twitter.com/nounsdao';
+  const twitterURL = externalURL(ExternalURL.twitter);
+  const discordURL = externalURL(ExternalURL.discord);
+  const etherscanURL = buildEtherscanAddressLink(config.tokenAddress, Network.mainnet);
 
   const location = useLocation();
   const useGreyBg = useAppSelector(state => state.application.useGreyBackground);
@@ -15,9 +19,13 @@ const Footer = () => {
     <Section bgColor={bgColor} fullWidth={false}>
       <footer className={classes.footerSignature}>
         <a href={twitterURL} target="_blank" rel="noreferrer">
-          made with
-          <img src={heartNoun} alt="heart noun" />
-          by the nounders
+          twitter
+        </a>
+        <a href={etherscanURL.toString()} target="_blank" rel="noreferrer">
+          etherscan
+        </a>
+        <a href={discordURL} target="_blank" rel="noreferrer">
+          discord
         </a>
       </footer>
     </Section>
