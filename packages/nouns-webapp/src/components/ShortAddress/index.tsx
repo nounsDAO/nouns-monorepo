89c97a2625793b1e633d5183368c36422e547b39
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-08-12 17:27:35 -0500

    Use reverse ENS look up to display ShortAddress comp

--- packages/nouns-webapp/src/components/ShortAddress/index.tsx
+++ packages/nouns-webapp/src/components/ShortAddress/index.tsx
@@ -1,6 +1,12 @@
+import { useReverseENSLookUp } from '../../utils/ensLookup';
+
 const ShortAddress: React.FC<{ address: string }> = props => {
   const { address } = props;
-  return <>{address && [address.substr(0, 6), address.substr(37, 7)].join('...')}</>;
+
+  const ens = useReverseENSLookUp(address);
+  const shortAddress = address && [address.substr(0, 4), address.substr(38, 4)].join('...');
+
+  return <>{ens ? ens : shortAddress}</>;
 };
 
 export default ShortAddress;
