8107499b2735d0816a78bd7d55f7dc05be8b64a3
devloper-eth <89419317+devloper-eth@users.noreply.github.com>
2021-08-25 22:04:53 -0700

    add eslint-disable-next-line and explain why

--- packages/nouns-webapp/src/hooks/useThrottledEffect.ts
+++ packages/nouns-webapp/src/hooks/useThrottledEffect.ts
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export const useThrottledEffect = (callback: React.EffectCallback, deps: React.DependencyList | undefined, delay: number) => {
+export const useThrottledEffect = (callback: React.EffectCallback, deps: React.DependencyList, delay: number) => {
 	const lastRan = useRef(Date.now());
+
 	useEffect(() => {
 		const handler = setTimeout(function () {
 			if (Date.now() - lastRan.current >= delay) {
@@ -12,7 +13,16 @@ export const useThrottledEffect = (callback: React.EffectCallback, deps: React.D
 		return () => {
 			clearTimeout(handler);
 		};
-	}, [...deps || [], delay]);
+
+		// react-hooks/exhaustive-deps complaints:
+		// 1. React Hook useEffect was passed a dependency list that is not an array literal. This means we can't statically verify whether you've passed the correct dependencies                                                                           react-hooks/exhaustive-deps
+		// 2. React Hook useEffect has missing dependencies: 'callback' and 'delay'. Either include them or remove the dependency array. If 'callback' changes too often, find the parent component that defines it and wrap that definition in useCallback  react-hooks/exhaustive-deps
+		//
+		// but:
+		// 1. passing `deps` here is perfectly fine.
+		// 2. callback, and delay are missing, but we don't care. 
+		// eslint-disable-next-line
+	}, deps)
 };
 
 export default useThrottledEffect;
