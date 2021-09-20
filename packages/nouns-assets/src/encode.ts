94fcf9d338188f726a46595d76ee6ed02ec11399
solimander <solimander19@gmail.com>
2021-09-20 14:08:39 +0100

    Consume SDK in assets package

---
+++ packages/nouns-assets/src/encode.ts
@@ -0,0 +1,23 @@
+import { PNGCollectionEncoder } from '../../nouns-sdk/src'; // TODO: '@nouns/sdk'
+import { readPngFile } from 'node-libpng';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const DESTINATION = path.join(__dirname, './output/image-data.json');
+
+const encode = async () => {
+  const encoder = new PNGCollectionEncoder();
+
+  const partfolders = ['1-bodies', '2-accessories', '3-heads', '4-glasses'];
+  for (const folder of partfolders) {
+    const folderpath = path.join(__dirname, './images', folder);
+    const files = await fs.readdir(folderpath);
+    for (const file of files) {
+      const image = await readPngFile(path.join(folderpath, file));
+      encoder.encodeImage(file.replace(/\.png$/, ''), image);
+    }
+  }
+  await encoder.writeToFile(DESTINATION);
+};
+
+encode();
