fa72ea890619c7c0ea123c324f980cca7ff279f1
solimander <solimander19@gmail.com>
2021-09-20 14:28:28 +0100

    Fix import issue

--- packages/nouns-assets/src/encode.ts
+++ packages/nouns-assets/src/encode.ts
@@ -1,4 +1,4 @@
-import { PNGCollectionEncoder } from '../../nouns-sdk/src'; // TODO: '@nouns/sdk'
+import { PNGCollectionEncoder } from '@nouns/sdk';
 import { readPngFile } from 'node-libpng';
 import { promises as fs } from 'fs';
 import path from 'path';
