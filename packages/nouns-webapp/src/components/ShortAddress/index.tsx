3cb4f2a176dacc189206d97d95441231ea411350
NOUN#13 <89419317+devloper-eth@users.noreply.github.com>
2021-09-24 20:47:27 -0700

    pseudo nouns.party ENS lookup

--- packages/nouns-webapp/src/components/ShortAddress/index.tsx
+++ packages/nouns-webapp/src/components/ShortAddress/index.tsx
@@ -6,6 +6,11 @@ const ShortAddress: React.FC<{ address: string }> = props => {
   const ens = useReverseENSLookUp(address);
   const shortAddress = address && [address.substr(0, 4), address.substr(38, 4)].join('...');
 
+  switch (address) {
+    case "0xd33f519291a5Ba56da1351243789C91a9C2a319e":
+      return <>party.nouns</>;
+  }
+
   return <>{ens ? ens : shortAddress}</>;
 };
 
