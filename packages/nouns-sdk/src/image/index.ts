aacd39c2e071c528c760d71445103068ea61b0d0
solimander <solimander19@gmail.com>
2021-10-05 20:25:00 -0600

    Add function used to build Noun SVGs

---
+++ packages/nouns-sdk/src/image/index.ts
@@ -0,0 +1,13 @@
+export { PNGCollectionEncoder } from './png-collection-encoder';
+export { buildNounSVG } from './svg-builder';
+export {
+  IEncoder,
+  Rect,
+  LineBounds,
+  ImageRow,
+  ImageRows,
+  ImageBounds,
+  RGBAColor,
+  DecodedImage,
+  EncodedImage,
+} from './types';
--- packages/nouns-sdk/src/index.ts
+++ packages/nouns-sdk/src/index.ts
@@ -1,12 +1,1 @@
-export { PNGCollectionEncoder } from './png-collection-encoder';
-export {
-  IEncoder,
-  Rect,
-  LineBounds,
-  ImageRow,
-  ImageRows,
-  ImageBounds,
-  RGBAColor,
-  DecodedImage,
-  EncodedImage,
-} from './types';
+export * from './image';
