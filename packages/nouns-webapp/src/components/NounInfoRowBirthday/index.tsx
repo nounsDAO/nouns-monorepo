30ceefbe251b1a82943d2ccb151d1eeffd125469
Brian <bcj26@case.edu>
2021-10-17 14:42:10 -0700

    fix sketchy css in noun info row birthday

--- packages/nouns-webapp/src/components/NounInfoRowBirthday/index.tsx
+++ packages/nouns-webapp/src/components/NounInfoRowBirthday/index.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@apollo/client";
 import { BigNumber } from "@ethersproject/bignumber";
+import { reduce } from "ramda";
 import React from "react";
 import { isNounderNoun } from "../../utils/nounderNoun";
 import { auctionQuery } from "../../wrappers/subgraph";
+
+import classes from './NounInfoRowBirthday.module.css';
 
 interface NounInfoRowBirthdayProps {
     nounId: number;
@@ -12,6 +15,9 @@ const NounInfoRowBirthday: React.FC<NounInfoRowBirthdayProps> = props => {
     const { nounId } = props;
 
 
+    // If the noun is a nounder noun, use the next noun to get the mint date.
+    // We do this because we use the auction start time to get the mint date and 
+    // nounder nouns do not have an auction start time.
     var nounIdForQuery = isNounderNoun(BigNumber.from(nounId)) ? nounId + 1 : nounId;
 
     const { loading, error, data } = useQuery(auctionQuery(nounIdForQuery));
@@ -28,15 +34,12 @@ const NounInfoRowBirthday: React.FC<NounInfoRowBirthdayProps> = props => {
     const birthday = new Date(data.auction.startTime * 1000);
 
     return (
-            <>
-            Born 
-            <div style={{
-                paddingLeft: 4,
-                fontFamily: 'PT Root UI Bold'
-            }}>
-                {monthNames[birthday.getUTCMonth()]} {birthday.getUTCDate()}, {birthday.getUTCFullYear()}
-            </div>
-      </>
+            <div className={classes.birthdayInfoContainer} >
+                Born 
+              <div className={classes.nounInfoRowBirthday}> 
+                  {monthNames[birthday.getUTCMonth()]} {birthday.getUTCDate()}, {birthday.getUTCFullYear()}
+              </div>
+          </div>
     );
 };
 
