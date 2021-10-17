5d15ad9f441ca451df948a41e2eadd2fe18aae6f
Brian <bcj26@case.edu>
2021-10-16 22:33:14 -0700

    Fix error where birthday failed on nounder noun

--- packages/nouns-webapp/src/components/NounInfoRowBirthday/index.tsx
+++ packages/nouns-webapp/src/components/NounInfoRowBirthday/index.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
+import { BigNumber } from "@ethersproject/bignumber";
 import React from "react";
+import { isNounderNoun } from "../../utils/nounderNoun";
 import { auctionQuery } from "../../wrappers/subgraph";
 
 interface NounInfoRowBirthdayProps {
@@ -9,17 +11,20 @@ interface NounInfoRowBirthdayProps {
 const NounInfoRowBirthday: React.FC<NounInfoRowBirthdayProps> = props => {
     const { nounId } = props;
 
-    const { loading, error, data } = useQuery(auctionQuery(nounId));
+
+    var nounIdForQuery = isNounderNoun(BigNumber.from(nounId)) ? nounId + 1 : nounId;
+
+    const { loading, error, data } = useQuery(auctionQuery(nounIdForQuery));
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
-  ];
+    ];
   
     if (loading) {
       return <></>;
     } else if (error) {
       return <div>Failed to fetch noun auction info</div>;
     }
-  
+    
     const birthday = new Date(data.auction.startTime * 1000);
 
     return (
