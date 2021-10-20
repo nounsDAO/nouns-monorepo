036f05bc594a515ad4fefecdbd4822f32b278ac9
andy <admazzola@gmail.com>
2021-10-20 13:17:04 -0400

    some tests pass now

---
+++ packages/nouns-contracts/test/tellertokens.test.ts
@@ -0,0 +1,122 @@
+import chai from 'chai';
+import { ethers } from 'hardhat';
+import { BigNumber as EthersBN, constants } from 'ethers';
+import { solidity } from 'ethereum-waffle';
+import {  TellerToken } from '../typechain';
+import { deployNounsToken, populateDescriptor } from './utils';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+chai.use(solidity);
+const { expect } = chai;
+
+describe('NounsToken', () => {
+  let tellerToken: TellerToken;
+  let deployer: SignerWithAddress;
+  let TreasuryDAO: SignerWithAddress;
+  let snapshotId: number;
+
+  before(async () => {
+    [deployer, TreasuryDAO] = await ethers.getSigners();
+    tellerToken = await deployNounsToken(deployer, TreasuryDAO.address, deployer.address);
+
+    const descriptor = await tellerToken.descriptor();
+
+    //await populateDescriptor(NounsDescriptorFactory.connect(descriptor, deployer));
+  });
+
+  beforeEach(async () => {
+    snapshotId = await ethers.provider.send('evm_snapshot', []);
+  });
+
+  afterEach(async () => {
+    await ethers.provider.send('evm_revert', [snapshotId]);
+  });
+
+  it('should allow the minter to mint a noun to itself and a reward noun to the noundersDAO', async () => {
+    const receipt = await (await tellerToken.mint()).wait();
+
+    const [, , , ownersNounCreated] = receipt.events || [];
+ 
+    expect(await tellerToken.ownerOf(1)).to.eq(deployer.address);
+    expect(ownersNounCreated?.event).to.eq('NounCreated');
+    expect(ownersNounCreated?.args?.tokenId).to.eq(0);
+    expect(ownersNounCreated?.args?.seed.length).to.equal(5);
+ 
+
+    ownersNounCreated?.args?.seed.forEach((item: EthersBN | number) => {
+      const value = typeof item !== 'number' ? item?.toNumber() : item;
+      expect(value).to.be.a('number');
+    });
+  });
+
+  it('should set symbol', async () => {
+    expect(await tellerToken.symbol()).to.eq('TCARD');
+  });
+
+  it('should set name', async () => {
+    expect(await tellerToken.name()).to.eq('TellerCard');
+  });
+
+  it('should allow minter to mint a noun to itself', async () => {
+    await (await tellerToken.mint()).wait();
+
+    const receipt = await (await tellerToken.mint()).wait();
+    const nounCreated = receipt.events?.[3];
+
+    expect(await tellerToken.ownerOf(2)).to.eq(deployer.address);
+    expect(nounCreated?.event).to.eq('NounCreated');
+    expect(nounCreated?.args?.tokenId).to.eq(2);
+    expect(nounCreated?.args?.seed.length).to.equal(5);
+
+    nounCreated?.args?.seed.forEach((item: EthersBN | number) => {
+      const value = typeof item !== 'number' ? item?.toNumber() : item;
+      expect(value).to.be.a('number');
+    });
+  });
+
+  it('should emit two transfer logs on mint', async () => {
+    const [, , creator, minter] = await ethers.getSigners();
+
+    await (await tellerToken.mint()).wait();
+
+    await (await tellerToken.setMinter(minter.address)).wait();
+    await (await tellerToken.transferOwnership(creator.address)).wait();
+
+    const tx = tellerToken.connect(minter).mint();
+
+    await expect(tx)
+      .to.emit(tellerToken, 'Transfer')
+      .withArgs(constants.AddressZero, creator.address, 2);
+    await expect(tx).to.emit(tellerToken, 'Transfer').withArgs(creator.address, minter.address, 2);
+  });
+
+  it('should allow minter to burn a noun', async () => {
+    await (await tellerToken.mint()).wait();
+
+    const tx = tellerToken.burn(0);
+    await expect(tx).to.emit(tellerToken, 'NounBurned').withArgs(0);
+  });
+
+  it('should revert on non-minter mint', async () => {
+    const account0AsNounErc721Account = tellerToken.connect(TreasuryDAO);
+    await expect(account0AsNounErc721Account.mint()).to.be.reverted;
+  });
+
+  describe('contractURI', async () => {
+    it('should return correct contractURI', async () => {
+      expect(await tellerToken.contractURI()).to.eq(
+        'ipfs://QmZi1n79FqWt2tTLwCqiy6nLM6xLGRsEPQ5JmReJQKNNzX',
+      );
+    });
+    it('should allow owner to set contractURI', async () => {
+      await tellerToken.setContractURIHash('ABC123');
+      expect(await tellerToken.contractURI()).to.eq('ipfs://ABC123');
+    });
+    it('should not allow non owner to set contractURI', async () => {
+      const [, nonOwner] = await ethers.getSigners();
+      await expect(tellerToken.connect(nonOwner).setContractURIHash('BAD')).to.be.revertedWith(
+        'Ownable: caller is not the owner',
+      );
+    });
+  });
+});
---
+++ packages/nouns-contracts/test/utils.ts
@@ -0,0 +1,211 @@
+import { ethers, network } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import {
+  TokenDescriptor,
+  TokenDescriptorFactory,
+  TellerToken,
+  TellerTokenFactory,
+  //NounsSeeder,
+  //NounsSeederFactory,
+  Weth,
+  WethFactory,
+} from '../typechain';
+import ImageData from '../files/image-data.json';
+import { Block } from '@ethersproject/abstract-provider';
+import { chunkArray } from '../utils';
+
+export type TestSigners = {
+  deployer: SignerWithAddress;
+  account0: SignerWithAddress;
+  account1: SignerWithAddress;
+  account2: SignerWithAddress;
+};
+
+export const getSigners = async (): Promise<TestSigners> => {
+  const [deployer, account0, account1, account2] = await ethers.getSigners();
+  return {
+    deployer,
+    account0,
+    account1,
+    account2,
+  };
+};
+
+export const deployTokenDescriptor = async (
+  deployer?: SignerWithAddress,
+): Promise<TokenDescriptor> => {
+  const signer = deployer || (await getSigners()).deployer;
+  const nftDescriptorLibraryFactory = await ethers.getContractFactory('NFTDescriptor', signer);
+  const nftDescriptorLibrary = await nftDescriptorLibraryFactory.deploy();
+  const nounsDescriptorFactory = new TokenDescriptorFactory(
+    
+    signer,
+  );
+
+  return nounsDescriptorFactory.deploy();
+};
+
+/*export const deployNounsSeeder = async (deployer?: SignerWithAddress): Promise<NounsSeeder> => {
+  const factory = new NounsSeederFactory(deployer || (await getSigners()).deployer);
+
+  return factory.deploy();
+};*/
+
+export const deployNounsToken = async (
+  deployer?: SignerWithAddress,
+  noundersDAO?: string,
+  minter?: string,
+  descriptor?: string,
+  seeder?: string,
+  proxyRegistryAddress?: string,
+): Promise<TellerToken> => {
+  const signer = deployer || (await getSigners()).deployer;
+  const factory = new TellerTokenFactory(signer);
+
+  return factory.deploy(
+    noundersDAO || signer.address,
+    minter || signer.address,
+    descriptor || (await deployTokenDescriptor(signer)).address,
+    //seeder || (await deployNounsSeeder(signer)).address,
+    proxyRegistryAddress || address(0),
+  );
+};
+
+export const deployWeth = async (deployer?: SignerWithAddress): Promise<Weth> => {
+  const factory = new WethFactory(deployer || (await await getSigners()).deployer);
+
+  return factory.deploy();
+};
+
+export const populateDescriptor = async (nounsDescriptor: TokenDescriptor): Promise<void> => {
+  const { bgcolors, palette, images } = ImageData;
+  const { bodies, accessories, heads, glasses } = images;
+
+  // Split up head and accessory population due to high gas usage
+  await Promise.all([
+    nounsDescriptor.addManyBackgrounds(bgcolors),
+    nounsDescriptor.addManyColorsToPalette(0, palette),
+    nounsDescriptor.addManyBodies(bodies.map(({ data }) => data)),
+    chunkArray(accessories, 10).map(chunk =>
+      nounsDescriptor.addManyAccessories(chunk.map(({ data }) => data)),
+    ),
+    chunkArray(heads, 10).map(chunk => nounsDescriptor.addManyHeads(chunk.map(({ data }) => data))),
+    nounsDescriptor.addManyGlasses(glasses.map(({ data }) => data)),
+  ]);
+};
+
+/**
+ * Return a function used to mint `amount` Nouns on the provided `token`
+ * @param token The Nouns ERC721 token
+ * @param amount The number of Nouns to mint
+ */
+export const MintTellerTokens = (
+  token: TellerToken,
+  burnNoundersTokens = true,
+): ((amount: number) => Promise<void>) => {
+  return async (amount: number): Promise<void> => {
+    for (let i = 0; i < amount; i++) {
+      await token.mint();
+    }
+    if (!burnNoundersTokens) return;
+
+    await setTotalSupply(token, amount);
+  };
+};
+
+/**
+ * Mints or burns tokens to target a total supply. Due to Nounders' rewards tokens may be burned and tokenIds will not be sequential
+ */
+export const setTotalSupply = async (token: TellerToken, newTotalSupply: number): Promise<void> => {
+  const totalSupply = (await token.totalSupply()).toNumber();
+
+  if (totalSupply < newTotalSupply) {
+    for (let i = 0; i < newTotalSupply - totalSupply; i++) {
+      await token.mint();
+    }
+    // If Nounder's reward tokens were minted totalSupply will be more than expected, so run setTotalSupply again to burn extra tokens
+    await setTotalSupply(token, newTotalSupply);
+  }
+
+  if (totalSupply > newTotalSupply) {
+    for (let i = newTotalSupply; i < totalSupply; i++) {
+      await token.burn(i);
+    }
+  }
+};
+
+// The following adapted from `https://github.com/compound-finance/compound-protocol/blob/master/tests/Utils/Ethereum.js`
+
+const rpc = <T = unknown>({
+  method,
+  params,
+}: {
+  method: string;
+  params?: unknown[];
+}): Promise<T> => {
+  return network.provider.send(method, params);
+};
+
+export const encodeParameters = (types: string[], values: unknown[]): string => {
+  const abi = new ethers.utils.AbiCoder();
+  return abi.encode(types, values);
+};
+
+export const blockByNumber = async (n: number | string): Promise<Block> => {
+  return rpc({ method: 'eth_getBlockByNumber', params: [n, false] });
+};
+
+export const increaseTime = async (seconds: number): Promise<unknown> => {
+  await rpc({ method: 'evm_increaseTime', params: [seconds] });
+  return rpc({ method: 'evm_mine' });
+};
+
+export const freezeTime = async (seconds: number): Promise<unknown> => {
+  await rpc({ method: 'evm_increaseTime', params: [-1 * seconds] });
+  return rpc({ method: 'evm_mine' });
+};
+
+export const advanceBlocks = async (blocks: number): Promise<void> => {
+  for (let i = 0; i < blocks; i++) {
+    await mineBlock();
+  }
+};
+
+export const blockNumber = async (parse = true): Promise<number> => {
+  const result = await rpc<number>({ method: 'eth_blockNumber' });
+  return parse ? parseInt(result.toString()) : result;
+};
+
+export const blockTimestamp = async (
+  n: number | string,
+  parse = true,
+): Promise<number | string> => {
+  const block = await blockByNumber(n);
+  return parse ? parseInt(block.timestamp.toString()) : block.timestamp;
+};
+
+export const setNextBlockTimestamp = async (n: number, mine = true): Promise<void> => {
+  await rpc({ method: 'evm_setNextBlockTimestamp', params: [n] });
+  if (mine) await mineBlock();
+};
+
+export const minerStop = async (): Promise<void> => {
+  await network.provider.send('evm_setAutomine', [false]);
+  await network.provider.send('evm_setIntervalMining', [0]);
+};
+
+export const minerStart = async (): Promise<void> => {
+  await network.provider.send('evm_setAutomine', [true]);
+};
+
+export const mineBlock = async (): Promise<void> => {
+  await network.provider.send('evm_mine');
+};
+
+export const chainId = async (): Promise<number> => {
+  return parseInt(await network.provider.send('eth_chainId'), 16);
+};
+
+export const address = (n: number): string => {
+  return `0x${n.toString(16).padStart(40, '0')}`;
+};
