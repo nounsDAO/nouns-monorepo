eb516ac66877c944b93ccec552dee86d734c1270
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-10-31 12:45:55 -0500

    Prevent full page refresh

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -119,7 +119,7 @@ const NavBar = () => {
             >
               DOCS
             </Nav.Link>
-            <Nav.Link href="/playground" className={classes.nounsNavLink}>
+            <Nav.Link as={Link} to="/playground" className={classes.nounsNavLink}>
               PLAYGROUND
             </Nav.Link>
             {activeAccount ? connectedContent : disconnectedContent}
