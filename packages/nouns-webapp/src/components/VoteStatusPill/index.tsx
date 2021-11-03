8ba0a174f9e89469299a8a759bd1a1d18163e0f6
Brian <bcj26@case.edu>
2021-11-02 20:20:23 -0400

    fix status bug

--- packages/nouns-webapp/src/components/VoteStatusPill/index.tsx
+++ packages/nouns-webapp/src/components/VoteStatusPill/index.tsx
@@ -2,28 +2,29 @@ import classes from './VoteStatusPill.module.css';
 
 interface VoteStatusPillProps {
   status: string;
+  text: string;
 }
 
 const VoteStatusPill: React.FC<VoteStatusPillProps> = props => {
-  const { status } = props;
+  const { status, text } = props;
   switch (status) {
     case 'success':
       return (
         <div className={`${classes.pass} ${classes.nounButton}`}>
           {' '}
-          <div className={classes.nounButtonContents}>Passed</div>
+          <div className={classes.nounButtonContents}>{text}</div>
         </div>
       );
     case 'failure':
       return (
         <div className={`${classes.fail} ${classes.nounButton}`}>
-          <div className={classes.nounButtonContents}>Failed</div>
+          <div className={classes.nounButtonContents}>{text}</div>
         </div>
       );
     default:
       return (
         <div className={`${classes.pending} ${classes.nounButton}`}>
-          <div className={classes.nounButtonContents}>Pending</div>
+          <div className={classes.nounButtonContents}>{text}</div>
         </div>
       );
   }
