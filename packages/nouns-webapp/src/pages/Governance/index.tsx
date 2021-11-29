8d576c72abbe98cc6c59c4929178210bff0cc50b
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-29 12:16:56 -0500

    Update min proposal threshold copy

--- packages/nouns-webapp/src/pages/Governance/index.tsx
+++ packages/nouns-webapp/src/pages/Governance/index.tsx
@@ -1,20 +1,25 @@
 import { Col } from 'react-bootstrap';
 import Section from '../../layout/Section';
-import { useAllProposals } from '../../wrappers/nounsDao';
+import { useAllProposals, useProposalThreshold } from '../../wrappers/nounsDao';
 import Proposals from '../../components/Proposals';
 import classes from './Governance.module.css';
 
 const GovernancePage = () => {
   const { data: proposals } = useAllProposals();
+  const threshold = useProposalThreshold();
+  const nounsCopy = threshold === 0 ? `1 Noun` : `${threshold} Nouns`;
 
   return (
     <Section fullWidth={true}>
       <Col lg={{ span: 8, offset: 2 }}>
         <h1 className={classes.heading}>Nouns DAO Governance</h1>
-        <p className={classes.subheading}>
-          Nouns govern NounsDAO. Nouns can vote on proposals or delegate their vote to a third
-          party. A minimum threshold of 1% of the total NOUN supply is required to submit proposals.
-        </p>
+        {threshold !== undefined && (
+          <p className={classes.subheading}>
+            {`Nouns govern NounsDAO. Nouns can vote on proposals or delegate their vote to a third
+            party. A minimum of ${nounsCopy} is required to submit
+            proposals.`}
+          </p>
+        )}
         <Proposals proposals={proposals} />
       </Col>
     </Section>
