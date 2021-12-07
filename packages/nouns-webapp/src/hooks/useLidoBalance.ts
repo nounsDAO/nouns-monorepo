2b6eaa4914c3714f67e725e518d0d9c83840d4b6
punk9999 <punk9999@protonmail.com>
2021-12-07 12:07:52 -0500

    `useLidoBalance` hook

---
+++ packages/nouns-webapp/src/hooks/useLidoBalance.ts
@@ -0,0 +1,31 @@
+import { useMemo, useEffect, useState } from 'react';
+import { Contract } from '@ethersproject/contracts';
+import { useEthers } from '@usedapp/core';
+import { utils, BigNumber } from 'ethers';
+import config from '../config';
+import ERC20 from '../libs/abi/ERC20.json';
+
+const {
+  addresses: { lidoToken, nounsDaoExecutor },
+} = config;
+
+const abi = new utils.Interface(ERC20);
+
+function useLidoBalance(): BigNumber | undefined {
+  const { library } = useEthers();
+
+  const [balance, setBalance] = useState(undefined);
+
+  const contract = useMemo(() => {
+    return library && lidoToken && new Contract(lidoToken, abi, library);
+  }, [library, lidoToken]);
+
+  useEffect(() => {
+    if (!contract || !nounsDaoExecutor) return;
+    contract.balanceOf(nounsDaoExecutor).then(setBalance);
+  }, [contract, nounsDaoExecutor]);
+
+  return balance;
+}
+
+export default useLidoBalance;
