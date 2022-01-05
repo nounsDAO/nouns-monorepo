83ad8de933a396c85210932319a22b72f65b3c7c
Brian <bcj26@case.edu>
2022-01-05 10:28:06 -0500

    update button css

--- packages/nouns-webapp/src/components/VotePageHeader/index.tsx
+++ packages/nouns-webapp/src/components/VotePageHeader/index.tsx
@@ -1,6 +1,23 @@
 import { Container, Col, Row } from 'react-bootstrap';
 import classes from './VotePageHeader.module.css';
-import { Link } from 'react-router-dom';
+
+
+const HeaderMain = () => {
+
+    return (
+        <Container className={classes.headerRow}>
+            <Col lg={7}>
+                <span>Proposal 17</span>
+                <h1>
+                    Fund an Engineer, Community manager, and Designer for Nouns
+                </h1>
+            </Col>
+            <Col lg={4}>
+                other stuff
+            </Col>
+        </Container>
+    );
+};
 
 const VotePageHeader = () => {
 
@@ -10,17 +27,15 @@ const VotePageHeader = () => {
     }
 
     return (
-        <Container>
-            <Col lg={1}>
-                <button
-            className={classes.leftArrowCool}
-            onClick={backButtonClickHandler}
-        >
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <button
+                className={classes.leftArrowCool}
+                onClick={backButtonClickHandler}
+            >
             ←
-        </button>
-            </Col>
-        </Container>
-
+            </button>
+            <HeaderMain />
+        </div>
     );
 };
 
