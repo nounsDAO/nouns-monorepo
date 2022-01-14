2c051d43f89ae3624011ff0a1637e789c0c51c86
Brian <bcj26@case.edu>
2022-01-13 20:57:16 -0800

    start on gov page

--- packages/nouns-webapp/src/components/Proposals/index.tsx
+++ packages/nouns-webapp/src/components/Proposals/index.tsx
@@ -1,5 +1,5 @@
 import { Proposal } from '../../wrappers/nounsDao';
-import { Alert, Button } from 'react-bootstrap';
+import { Alert, Button, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import ProposalStatus from '../ProposalStatus';
 import classes from './Proposals.module.css';
 import { useHistory } from 'react-router-dom';
@@ -7,17 +7,33 @@ import { useHistory } from 'react-router-dom';
 const Proposals = ({ proposals }: { proposals: Proposal[] }) => {
   const history = useHistory();
 
+  // TODO get wallet connected state and noun balance of addr
+  const renderTooltip = (props: any) => (
+    <Tooltip id="button-tooltip" {...props}>
+      <div style={{
+        fontFamily: 'PT Root UI'
+      }}>
+        You must have at least 1 Noun to submit a proposal
+      </div>
+    </Tooltip>
+  );
+
   return (
     <div className={classes.proposals}>
       <div>
         <h3 className={classes.heading}>Proposals</h3>
-        <Button
-          variant="success"
-          className={classes.createProposalLink}
-          onClick={() => history.push('create-proposal')}
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 0, hide: 0 }}
+          overlay={renderTooltip}
         >
-          Create Proposal
-        </Button>
+          <Button
+            className={classes.generateBtn}
+            onClick={() => history.push('create-proposal')}
+          >
+            Submit Proposal
+          </Button>
+        </OverlayTrigger>
       </div>
       {proposals?.length ? (
         proposals
@@ -25,17 +41,16 @@ const Proposals = ({ proposals }: { proposals: Proposal[] }) => {
           .reverse()
           .map((p, i) => {
             return (
-              <Button
+              <div
                 className={classes.proposalLink}
-                variant="dark"
                 onClick={() => history.push(`/vote/${p.id}`)}
                 key={i}
               >
                 <span>
-                  <span>{p.id}.</span> <span>{p.title}</span>
+                  <span style={{color: '#8C8D92', marginRight: '.5rem'}}>{p.id}</span> <span>{p.title}</span>
                 </span>
                 <ProposalStatus status={p.status}></ProposalStatus>
-              </Button>
+              </div>
             );
           })
       ) : (
--- packages/nouns-webapp/src/pages/Governance/index.tsx
+++ packages/nouns-webapp/src/pages/Governance/index.tsx
@@ -1,4 +1,4 @@
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Section from '../../layout/Section';
 import { useAllProposals, useProposalThreshold } from '../../wrappers/nounsDao';
 import Proposals from '../../components/Proposals';
@@ -13,11 +13,56 @@ const GovernancePage = () => {
   return (
     <Section fullWidth={true}>
       <Col lg={{ span: 8, offset: 2 }}>
-        <h1 className={classes.heading}>Nouns DAO Governance</h1>
+        <Row className={classes.headerRow}>
+          <span>Governance</span>
+          <h1>Nouns DAO</h1>
+        </Row>
         <p className={classes.subheading}>
-          Nouns govern NounsDAO. Nouns can vote on proposals or delegate their vote to a third
-          party. A minimum of {nounThresholdCopy} is required to submit proposals.
+          Nouns govern <span className={classes.boldText}>NounsDAO</span>. Nouns can vote on proposals or delegate their vote to a third
+          party. A minimum of <span className={classes.boldText}>{nounThresholdCopy}</span> is required to submit proposals.
         </p>
+
+        <div style={{
+          marginBottom: '3rem',
+          borderRadius: '20px',
+          border: '1px solid #E2E3E8'
+        }}>
+          <Row>
+            <Col lg={9} style={{
+                borderRight: '1px solid #E2E3E8',
+                paddingTop: '1rem',
+                paddingBottom: '1rem',
+                paddingLeft: '2rem',
+                paddingRight: '2rem'
+              }}>
+                <Row className={classes.headerRow}>
+                  <span>Treasury</span>
+                </Row>
+                <Row>
+                    <Col className={classes.headerRow} lg={3} style={{
+                      borderRight: '1px solid #E2E3E8'
+                    }}>
+                      <h1>Ξ 1234</h1>
+                    </Col>
+                    <Col>
+                      <h1>$ 25,000,000</h1>
+                    </Col>
+                </Row>
+            </Col>
+            <Col style={{
+              paddingTop: '1rem',
+              paddingBottom: '1rem',
+              paddingLeft: '2rem',
+              paddingRight: '2rem',
+              fontFamily: 'PT Root UI'
+            }}>
+              This treasury exists for <span className={classes.boldText}>NounsDAO</span> participants to allocate resources
+              for the long-term growth and
+              prosperity of the Nouns project.
+            </Col>
+          </Row>
+        </div>
+
         <Proposals proposals={proposals} />
       </Col>
     </Section>
