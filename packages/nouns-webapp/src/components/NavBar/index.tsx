b2bb2acccda9219f62ad7eac3c9e77835f8a0cc4
pbrianandj <bcj26@case.edu>
2022-01-28 21:31:40 -0800

    FIX: auction info below the fold on mobile

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -23,6 +23,7 @@ import NavWallet from '../NavWallet';
 const NavBar = () => {
   const activeAccount = useAppSelector(state => state.account.activeAccount);
   const stateBgColor = useAppSelector(state => state.application.stateBackgroundColor);
+  const isCool = useAppSelector(state => state.application.isCoolBackground);
   const history = useHistory();
   const ethBalance = useEtherBalance(config.addresses.nounsDaoExecutor);
   const lidoBalanceAsETH = useLidoBalance();
@@ -34,11 +35,9 @@ const NavBar = () => {
     history.location.pathname.includes('/noun') ||
     history.location.pathname.includes('/auction');
 
-  const greyBg = '#d5d7e1';
-
   const nonWalletButtonStyle = !useStateBg
     ? NavBarButtonStyle.WHITE_INFO
-    : stateBgColor === greyBg
+    : isCool
     ? NavBarButtonStyle.COOL_INFO
     : NavBarButtonStyle.WARM_INFO;
 
