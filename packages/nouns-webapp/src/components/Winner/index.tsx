a4ace9c6b5153d274eec29022647ce0a45df4e7a
pbrianandj <bcj26@case.edu>
2022-02-02 22:56:36 -0800

    Update winner You state per seneca

--- packages/nouns-webapp/src/components/Winner/index.tsx
+++ packages/nouns-webapp/src/components/Winner/index.tsx
@@ -4,6 +4,7 @@ import classes from './Winner.module.css';
 import ShortAddress from '../ShortAddress';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
+import { isMobileScreen } from '../../utils/isMobile';
 
 interface WinnerProps {
   winner: string;
@@ -14,46 +15,69 @@ const Winner: React.FC<WinnerProps> = props => {
   const { winner, isNounders } = props;
   const activeAccount = useAppSelector(state => state.account.activeAccount);
 
+  const isCool = useAppSelector(state => state.application.isCoolBackground);
+  const isMobile = isMobileScreen();
   const nonNounderNounContent =
     activeAccount !== undefined &&
     activeAccount.toLocaleLowerCase() === winner.toLocaleLowerCase() ? (
       <Row className={classes.youSection}>
-        <Row>You!</Row>
-        <Link to="/verify" className={classes.verifyLink}>
-          <Button className={classes.verifyButton}>Get Verified</Button>
-        </Link>
+        <Col lg={4} className={classes.youCopy}>
+          <h2
+            className={classes.winnerContent}
+            style={{
+              color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+            }}
+          >
+            You
+          </h2>
+        </Col>
+        {!isMobile && (
+          <Col>
+            <Link to="/verify" className={classes.verifyLink}>
+              <Button className={classes.verifyButton}>Get Verified</Button>
+            </Link>
+          </Col>
+        )}
       </Row>
     ) : (
       <ShortAddress size={40} address={winner} avatar={true} />
     );
 
-  const isCool = useAppSelector(state => state.application.isCoolBackground);
   const nounderNounContent = <h2>nounders.eth</h2>;
 
   return (
-    <Row className={clsx(classes.wrapper, classes.section)}>
-      <Col xs={1} lg={12} className={classes.leftCol}>
-        <h4
-          style={{
-            fontFamily: 'PT Root UI',
-            fontWeight: 'bold',
-            color: isCool ? 'var(--brand-cool-light-text)' : 'var(--brand-warm-light-text)',
-          }}
-        >
-          Winner
-        </h4>
-      </Col>
-      <Col xs="auto" lg={12}>
-        <h2
-          className={classes.winnerContent}
-          style={{
-            color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
-          }}
-        >
-          {isNounders ? nounderNounContent : nonNounderNounContent}
-        </h2>
-      </Col>
-    </Row>
+    <>
+      <Row className={clsx(classes.wrapper, classes.section)}>
+        <Col xs={1} lg={12} className={classes.leftCol}>
+          <h4
+            style={{
+              fontFamily: 'PT Root UI',
+              fontWeight: 'bold',
+              color: isCool ? 'var(--brand-cool-light-text)' : 'var(--brand-warm-light-text)',
+            }}
+          >
+            Winner
+          </h4>
+        </Col>
+        <Col xs="auto" lg={12}>
+          <h2
+            className={classes.winnerContent}
+            style={{
+              color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+            }}
+          >
+            {isNounders ? nounderNounContent : nonNounderNounContent}
+          </h2>
+        </Col>
+      </Row>
+      {isMobile && (
+        <Row>
+          <Link to="/verify" className={classes.verifyLink}>
+            <Button className={classes.verifyButton}>Get Verified</Button>
+          </Link>
+        </Row>
+      )}
+    </>
   );
 };
 
