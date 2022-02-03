8c5a272c00c6759f1dd79e6b0b09f5c83453c057
pbrianandj <bcj26@case.edu>
2022-02-03 13:05:11 -0800

    fix auction time left / end time mobile alignment

--- packages/nouns-webapp/src/components/AuctionTimer/index.tsx
+++ packages/nouns-webapp/src/components/AuctionTimer/index.tsx
@@ -3,7 +3,7 @@ import duration from 'dayjs/plugin/duration';
 import { Auction } from '../../wrappers/nounsAuction';
 import classes from './AuctionTimer.module.css';
 import { useState, useEffect, useRef } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { useAppSelector } from '../../hooks';
 import clsx from 'clsx';
 
@@ -53,61 +53,64 @@ const AuctionTimer: React.FC<{
   if (!auction) return null;
 
   return (
-    <div onClick={() => setTimerToggle(!timerToggle)} className={classes.auctionTimerSection}>
-      <Container className={clsx(classes.wrapper, classes.container)}>
-        <Row className={classes.section}>
-          <Col xs={timerToggle ? 4 : 'auto'} lg={12} className={classes.leftCol}>
-            <h4
-              className={classes.title}
-              style={{
-                color: isCool ? 'var(--brand-cool-light-text)' : 'var(--brand-warm-light-text)',
-              }}
-            >
-              {timerToggle
-                ? window.innerWidth < 992
-                  ? auctionContentShort
-                  : auctionContentLong
-                : `Ends on ${endTime.format('MMM Do')} at`}
-            </h4>
-          </Col>
-          <Col xs="auto" lg={12}>
-            <h2
-              className={classes.timerWrapper}
-              style={{
-                color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
-              }}
-            >
-              {timerToggle ? (
-                <>
-                  <div className={classes.timerSection}>
-                    <span>
-                      {`${Math.floor(timerDuration.hours())}`}
-                      <span className={classes.small}>h</span>
-                    </span>
-                  </div>
-                  <div className={classes.timerSection}>
-                    <span>
-                      {`${flooredMinutes}`}
-                      <span className={classes.small}>m</span>
-                    </span>
-                  </div>
-                  <div className={classes.timerSectionFinal}>
-                    <span>
-                      {`${flooredSeconds}`}
-                      <span className={classes.small}>s</span>
-                    </span>
-                  </div>
-                </>
-              ) : (
-                <div className={clsx(classes.timerSection, classes.clockSection)}>
-                  <span>{endTime.format('h:mm:ss a')}</span>
-                </div>
-              )}
-            </h2>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Row
+      className={clsx(classes.wrapper, classes.section)}
+      onClick={() => setTimerToggle(!timerToggle)}
+    >
+      <Col xs={6} lg={12} className={classes.leftCol}>
+        <h4
+          style={{
+            color: isCool ? 'var(--brand-cool-light-text)' : 'var(--brand-warm-light-text)',
+          }}
+        >
+          {timerToggle
+            ? window.innerWidth < 992
+              ? auctionContentShort
+              : auctionContentLong
+            : `Ends on ${endTime.format('MMM Do')} at`}
+        </h4>
+      </Col>
+      <Col xs="auto" lg={12}>
+        {timerToggle ? (
+          <h2
+            className={clsx(classes.timerWrapper, classes.timeLeft)}
+            style={{
+              color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+            }}
+          >
+            <div className={classes.timerSection}>
+              <span>
+                {`${Math.floor(timerDuration.hours())}`}
+                <span className={classes.small}>h</span>
+              </span>
+            </div>
+            <div className={classes.timerSection}>
+              <span>
+                {`${flooredMinutes}`}
+                <span className={classes.small}>m</span>
+              </span>
+            </div>
+            <div className={classes.timerSectionFinal}>
+              <span>
+                {`${flooredSeconds}`}
+                <span className={classes.small}>s</span>
+              </span>
+            </div>
+          </h2>
+        ) : (
+          <h2
+            className={classes.timerWrapper}
+            style={{
+              color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+            }}
+          >
+            <div className={clsx(classes.timerSection, classes.clockSection)}>
+              <span>{endTime.format('h:mm:ss a')}</span>
+            </div>
+          </h2>
+        )}
+      </Col>
+    </Row>
   );
 };
 
