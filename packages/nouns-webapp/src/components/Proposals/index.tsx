2659ec527bd087a95183669e8dbcfa3f71cc7611
pbrianandj <bcj26@case.edu>
2022-02-14 11:36:58 -0800

    Update UX of list of proposals on /vote

--- packages/nouns-webapp/src/components/Proposals/index.tsx
+++ packages/nouns-webapp/src/components/Proposals/index.tsx
@@ -1,41 +1,65 @@
-import { Proposal } from '../../wrappers/nounsDao';
+import { Proposal, useProposalThreshold } from '../../wrappers/nounsDao';
 import { Alert, Button } from 'react-bootstrap';
 import ProposalStatus from '../ProposalStatus';
 import classes from './Proposals.module.css';
 import { useHistory } from 'react-router-dom';
+import { useEthers, useTokenBalance } from '@usedapp/core';
+import config from '../../config';
+import { isMobileScreen } from '../../utils/isMobile';
 
 const Proposals = ({ proposals }: { proposals: Proposal[] }) => {
   const history = useHistory();
+
+  const { account } = useEthers();
+  const connectedAccountNounBalance = Number(useTokenBalance(config.addresses.nounsToken, account));
+
+  const threshold = useProposalThreshold();
+  const nounsRequired = threshold !== undefined ? threshold + 1 : '...';
+  const nounThresholdCopy = `${nounsRequired} ${threshold === 0 ? 'Noun' : 'Nouns'}`;
+
+  const isMobile = isMobileScreen();
+
+  const nullStateCopy = () => {
+    if (account !== null) {
+      return `You must have at least ${nounThresholdCopy} to make a proposal.`;
+    }
+    return 'Connect wallet to make a proposal.';
+  };
 
   return (
     <div className={classes.proposals}>
       <div>
         <h3 className={classes.heading}>Proposals</h3>
-        <Button
-          variant="success"
-          className={classes.createProposalLink}
-          onClick={() => history.push('create-proposal')}
-        >
-          Create Proposal
-        </Button>
+        {account !== undefined && connectedAccountNounBalance > 0 ? (
+          <Button className={classes.generateBtn} onClick={() => history.push('create-proposal')}>
+            Submit Proposal
+          </Button>
+        ) : (
+          <div className="d-flex">
+            {!isMobile && <div className={classes.nullStateCopy}>{nullStateCopy()}</div>}
+            <div className={classes.nullBtnWrapper}>
+              <Button className={classes.generateBtnDisabled}>Submit Proposal</Button>
+            </div>
+          </div>
+        )}
       </div>
+      {isMobile && <div className={classes.nullStateCopy}>{nullStateCopy()}</div>}
       {proposals?.length ? (
         proposals
           .slice(0)
           .reverse()
           .map((p, i) => {
             return (
-              <Button
+              <div
                 className={classes.proposalLink}
-                variant="dark"
                 onClick={() => history.push(`/vote/${p.id}`)}
                 key={i}
               >
-                <span>
-                  <span>{p.id}.</span> <span>{p.title}</span>
+                <span className={classes.proposalTitle}>
+                  <span className={classes.proposalId}>{p.id}</span> <span>{p.title}</span>
                 </span>
                 <ProposalStatus status={p.status}></ProposalStatus>
-              </Button>
+              </div>
             );
           })
       ) : (
