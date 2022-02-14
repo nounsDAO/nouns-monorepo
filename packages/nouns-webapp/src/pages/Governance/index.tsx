5eb1f7bee896bf46341e9ab60b19b3953b122a36
pbrianandj <bcj26@case.edu>
2022-02-14 11:28:32 -0800

    Update Goverancen page above the fold (copy, treasury balance, description of DAO goals, etc)

--- packages/nouns-webapp/src/pages/Governance/index.tsx
+++ packages/nouns-webapp/src/pages/Governance/index.tsx
@@ -1,8 +1,12 @@
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Section from '../../layout/Section';
 import { useAllProposals, useProposalThreshold } from '../../wrappers/nounsDao';
 import Proposals from '../../components/Proposals';
 import classes from './Governance.module.css';
+import { utils } from 'ethers/lib/ethers';
+import { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import { useTreasuryBalance } from '../../hooks/useTreasuryBalance';
 
 const GovernancePage = () => {
   const { data: proposals } = useAllProposals();
@@ -10,14 +14,70 @@ const GovernancePage = () => {
   const nounsRequired = threshold !== undefined ? threshold + 1 : '...';
   const nounThresholdCopy = `${nounsRequired} ${threshold === 0 ? 'Noun' : 'Nouns'}`;
 
+  const treasuryBalance = useTreasuryBalance();
+  const [treasuryBalanceUSD, setTreasuryBalanceUSD] = useState(0);
+  const [ethUSDConversionRate, setEthUSDConversionRate] = useState(0);
+
+  useEffect(() => {
+    async function getEthPriceInUSD() {
+      const response = await fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD',
+      );
+      const json = await response.json();
+      setEthUSDConversionRate(Number(json?.USD));
+    }
+
+    if (!treasuryBalance) {
+      return;
+    }
+    getEthPriceInUSD();
+
+    setTreasuryBalanceUSD(Number(utils.formatEther(treasuryBalance)) * ethUSDConversionRate);
+  }, [treasuryBalance, ethUSDConversionRate]);
+
   return (
-    <Section fullWidth={true}>
-      <Col lg={{ span: 8, offset: 2 }}>
-        <h1 className={classes.heading}>Nouns DAO Governance</h1>
+    <Section fullWidth={false} className={classes.section}>
+      <Col lg={10} className={classes.wrapper}>
+        <Row className={classes.headerRow}>
+          <span>Governance</span>
+          <h1>Nouns DAO</h1>
+        </Row>
         <p className={classes.subheading}>
-          Nouns govern NounsDAO. Nouns can vote on proposals or delegate their vote to a third
-          party. A minimum of {nounThresholdCopy} is required to submit proposals.
+          Nouns govern <span className={classes.boldText}>NounsDAO</span>. Nouns can vote on
+          proposals or delegate their vote to a third party. A minimum of{' '}
+          <span className={classes.boldText}>{nounThresholdCopy}</span> is required to submit
+          proposals.
         </p>
+
+        <Row className={classes.treasuryInfoCard}>
+          <Col lg={9} className={classes.treasuryAmtWrapper}>
+            <Row className={classes.headerRow}>
+              <span>Treasury</span>
+            </Row>
+            <Row>
+              <Col className={clsx(classes.headerRow, classes.ethTreasuryAmt)} lg={4}>
+                <h1 className={classes.ethSymbol}>Ξ</h1>
+                <h1>
+                  {treasuryBalance &&
+                    Number(Number(utils.formatEther(treasuryBalance)).toFixed(0)).toLocaleString(
+                      'en-US',
+                    )}
+                </h1>
+              </Col>
+              <Col>
+                <h1 className={classes.usdBalance}>
+                  ${' '}
+                  {treasuryBalanceUSD &&
+                    Number(treasuryBalanceUSD.toFixed(0)).toLocaleString('en-US')}
+                </h1>
+              </Col>
+            </Row>
+          </Col>
+          <Col className={classes.treasuryInfoText}>
+            This treasury exists for <span className={classes.boldText}>NounsDAO</span> participants
+            to allocate resources for the long-term growth and prosperity of the Nouns project.
+          </Col>
+        </Row>
         <Proposals proposals={proposals} />
       </Col>
     </Section>
