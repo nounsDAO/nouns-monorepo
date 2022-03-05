fdaa8a5777c8ceb18731595f16f255d23429f33b
ryeshrimp <ryeshrimp@protonmail.com>
2022-03-05 11:01:08 -0500

    Use blockNumber instead of blockno and add description

--- packages/nouns-webapp/src/hooks/useBlockTimestamp.ts
+++ packages/nouns-webapp/src/hooks/useBlockTimestamp.ts
@@ -1,20 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useEthers } from '@usedapp/core';
 
-export function useBlockTimestamp(blockno: number | undefined): number | undefined {
+/**
+ * A function that takes a block number from the chain and returns the timestamp of when the block occurred.
+ * @param blockNumber target block number to retrieve the timestamp for
+ * @returns unix timestamp of block number
+ */
+export function useBlockTimestamp(blockNumber: number | undefined): number | undefined {
   const { library } = useEthers();
   const [blockTimestamp, setBlockTimestamp] = useState<number | undefined>();
 
   useEffect(() => {
-
     async function updateBlockTimestamp() {
-      if (!blockno) return;
-      const blockData = await library?.getBlock(blockno);
+      if (!blockNumber) return;
+      const blockData = await library?.getBlock(blockNumber);
       setBlockTimestamp(blockData?.timestamp || undefined);
     }
-    
+
     updateBlockTimestamp();
-  }, [blockno, library]);
+  }, [blockNumber, library]);
 
   return blockTimestamp;
 }
