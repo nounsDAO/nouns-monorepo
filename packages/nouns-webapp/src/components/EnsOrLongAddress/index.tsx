bbcfc5cb665660e5d0ece1f5cfb71e20c35db44a
pbrianandj <bcj26@case.edu>
2022-05-04 15:37:25 -0700

    add comment to EnsOrLongAddress

--- packages/nouns-webapp/src/components/EnsOrLongAddress/index.tsx
+++ packages/nouns-webapp/src/components/EnsOrLongAddress/index.tsx
@@ -3,6 +3,10 @@ import { useReverseENSLookUp } from "../../utils/ensLookup";
 interface EnsOrLongAddressProps {
     address: string;
 }
+
+/**
+ * Resolves ENS for address if one exists, otherwise falls back to full address
+ */
 const EnsOrLongAddress: React.FC<EnsOrLongAddressProps> = props => {
     const {address} = props;
     const ens = useReverseENSLookUp(address);
