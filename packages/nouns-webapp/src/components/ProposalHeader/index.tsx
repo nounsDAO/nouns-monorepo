f3a83561552fc98fcadedc87fa7c710aa95eb986
pbrianandj <bcj26@case.edu>
2022-05-09 13:58:06 -0700

    add ENS everywhere and proposer by line

--- packages/nouns-webapp/src/components/ProposalHeader/index.tsx
+++ packages/nouns-webapp/src/components/ProposalHeader/index.tsx
@@ -11,6 +11,9 @@ import { useUserVotesAsOfBlock } from '../../wrappers/nounToken';
 import { useBlockTimestamp } from '../../hooks/useBlockTimestamp';
 import { Trans } from '@lingui/macro';
 import { i18n } from '@lingui/core';
+import { buildEtherscanAddressLink } from '../../utils/etherscan';
+import { transactionLink } from '../ProposalContent';
+import ShortAddress from '../ShortAddress';
 
 interface ProposalHeaderProps {
   proposal: Proposal;
@@ -101,6 +104,24 @@ const ProposalHeader: React.FC<ProposalHeaderProps> = props => {
             </div>
           </div>
         </div>
+        <div className={classes.byLineWrapper}>
+        <h3>Proposed by:</h3> 
+
+        <div className={classes.byLineContentWrapper}>
+        <h3>
+
+          <a href={buildEtherscanAddressLink(proposal.proposer || "")} target="_blank" rel="noreferrer">
+            <ShortAddress address={proposal.proposer || ""} avatar={false}/>
+          </a>
+
+          <span className={classes.propTransactionWrapper}>
+            at <span className={classes.propTransactionHash}>
+              {transactionLink(proposal.transactionHash)}
+            </span>
+          </span> 
+        </h3>
+        </div>
+      </div>
         {!isMobile && (
           <div className="d-flex justify-content-end align-items-end">
             {isActiveForVoting && voteButton}
