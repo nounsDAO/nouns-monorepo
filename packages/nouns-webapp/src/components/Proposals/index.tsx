b2247c12e5f805f6722c1993829d88465d388c92
pbrianandj <bcj26@case.edu>
2022-05-09 22:43:46 -0700

    Add proposal state change countdown (no i18n)

--- packages/nouns-webapp/src/components/Proposals/index.tsx
+++ packages/nouns-webapp/src/components/Proposals/index.tsx
@@ -1,21 +1,59 @@
-import { Proposal } from '../../wrappers/nounsDao';
+import { Proposal, ProposalState } from '../../wrappers/nounsDao';
 import { Alert, Button } from 'react-bootstrap';
 import ProposalStatus from '../ProposalStatus';
 import classes from './Proposals.module.css';
 import { useHistory } from 'react-router-dom';
-import { useEthers } from '@usedapp/core';
+import { useBlockNumber, useEthers } from '@usedapp/core';
 import { isMobileScreen } from '../../utils/isMobile';
 import clsx from 'clsx';
 import { useUserVotes } from '../../wrappers/nounToken';
 import { Trans } from '@lingui/macro';
 import { i18n } from '@lingui/core';
+import { ClockIcon } from '@heroicons/react/solid';
+import proposalStatusClasses from '../ProposalStatus/ProposalStatus.module.css';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+const getCountdownCopy = (proposal: Proposal, currentBlock: number) => {
+  const AVERAGE_BLOCK_TIME_IN_SECS = 13;
+  const timestamp = Date.now();
+  const startDate =
+    proposal && timestamp && currentBlock
+      ? dayjs(timestamp).add(
+          AVERAGE_BLOCK_TIME_IN_SECS * (proposal.startBlock - currentBlock),
+          'seconds',
+        )
+      : undefined;
+
+  const endDate =
+    proposal && timestamp && currentBlock
+      ? dayjs(timestamp).add(
+          AVERAGE_BLOCK_TIME_IN_SECS * (proposal.endBlock - currentBlock),
+          'seconds',
+        )
+      : undefined;
+
+  const expiresDate = proposal && dayjs(proposal.eta).add(14, 'days');
+
+  const now = dayjs();
+
+  if (startDate?.isBefore(now) && endDate?.isAfter(now)) {
+    return `Ends ${endDate.fromNow()}`;
+  }
+  if (endDate?.isBefore(now)) {
+    return `Expires ${expiresDate.fromNow()}`;
+  }
+  return `Starts ${dayjs(startDate).fromNow()} `;
+};
 
 const Proposals = ({ proposals }: { proposals: Proposal[] }) => {
   const history = useHistory();
 
   const { account } = useEthers();
   const connectedAccountNounVotes = useUserVotes() || 0;
-
+  const currentBlock = useBlockNumber();
   const isMobile = isMobileScreen();
 
   const nullStateCopy = () => {
@@ -54,18 +92,50 @@ const Proposals = ({ proposals }: { proposals: Proposal[] }) => {
           .slice(0)
           .reverse()
           .map((p, i) => {
+            const isPropInStateToHaveCountDown =
+              p.status === ProposalState.PENDING ||
+              p.status === ProposalState.ACTIVE ||
+              p.status === ProposalState.SUCCEEDED ||
+              p.status === ProposalState.QUEUED;
             return (
               <div
-                className={classes.proposalLink}
+                className={clsx(
+                  classes.proposalLink,
+                  isPropInStateToHaveCountDown ? classes.proposalLinkWithCountdown : '',
+                )}
                 onClick={() => history.push(`/vote/${p.id}`)}
                 key={i}
               >
                 <span className={classes.proposalTitle}>
-                  <span className={classes.proposalId}>{i18n.number(parseInt(p.id || '0'))}</span>{' '}
-                  <span>{p.title}</span>
+                  <span className={classes.proposalId}>{p.id}</span> <span>{p.title}</span>
                 </span>
-                <div className={classes.proposalStatusWrapper}>
-                  <ProposalStatus status={p.status}></ProposalStatus>
+
+                <div
+                  className={isPropInStateToHaveCountDown ? classes.proposalInfoPillsWrapper : ''}
+                >
+                  {isPropInStateToHaveCountDown && (
+                    <div className={classes.proposalStatusWrapper}>
+                      <div
+                        className={clsx(
+                          proposalStatusClasses.proposalStatus,
+                          classes.countdownPill,
+                        )}
+                      >
+                        <div className={classes.countdownPillContentWrapper}>
+                          <span className={classes.countdownPillClock}>
+                            <ClockIcon height={16} width={16} />
+                          </span>{' '}
+                          <span className={classes.countdownPillText}>
+                            {getCountdownCopy(p, currentBlock || 0)}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
+                  <div className={classes.proposalStatusWrapper}>
+                    <ProposalStatus status={p.status}></ProposalStatus>
+                  </div>
                 </div>
               </div>
             );
