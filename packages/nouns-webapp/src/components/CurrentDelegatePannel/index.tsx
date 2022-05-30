91733556f4d7fb59ccf7f4978f0165e5eb9154bb
pbrianandj <bcj26@case.edu>
2022-05-30 16:54:20 -0400

    small clean up in current delegate modal

--- packages/nouns-webapp/src/components/CurrentDelegatePannel/index.tsx
+++ packages/nouns-webapp/src/components/CurrentDelegatePannel/index.tsx
@@ -14,34 +14,38 @@ interface CurrentDelegatePannelProps {
 const CurrentDelegatePannel: React.FC<CurrentDelegatePannelProps> = props => {
   const { onPrimaryBtnClick, onSecondaryBtnClick } = props;
 
-  const { account } = useEthers();
-  const shortAccount = useShortAddress(account || '');
+  const { account: maybeAccount } = useEthers();
+  const account = maybeAccount ?? '';
+  const shortAccount = useShortAddress(account);
 
   return (
     <>
       <div className={classes.wrapper}>
-        <h1 className={classes.title}>Delegation</h1>
+        <h1 className={classes.title}>
+          <Trans>
+          Delegation
+          </Trans>
+        </h1>
 
         <p className={classes.copy}>
+          <Trans>
           Noun votes are not transferable, but are <span className={classes.emph}>delegatable</span>
           , which means you can assign your vote to someone else as long as you own your Noun.
+          </Trans>
         </p>
       </div>
 
-      {/* Current delegate */}
       <div className={classes.contentWrapper}>
         <div className={classes.current}>Current</div>
 
-        {/* Current delegate component */}
         <div className={classes.delegateInfoWrapper}>
           <div className={classes.ens}>
-            <ShortAddress address={account || ''} avatar={true} size={39} />
+            <ShortAddress address={account} avatar={true} size={39} />
           </div>
           <div className={classes.shortAddress}>{shortAccount}</div>
         </div>
       </div>
 
-      {/* Button section */}
       <div className={classes.buttonWrapper}>
         <NavBarButton
           buttonText={<Trans>Close</Trans>}
