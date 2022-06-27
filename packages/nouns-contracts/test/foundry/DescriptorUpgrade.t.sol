c831476ebe65b1505e7551cf1aa0c43d7027d65f
eladmallel <eladmallel@gmail.com>
2022-06-27 16:45:39 -0500

    add descriptor upgrade test

---
+++ packages/nouns-contracts/test/foundry/DescriptorUpgrade.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+
+import 'forge-std/Test.sol';
+import { DescriptorHelpers } from './helpers/DescriptorHelpers.sol';
+import { NounsToken } from '../../contracts/NounsToken.sol';
+import { NounsDescriptor } from '../../contracts/NounsDescriptor.sol';
+import { NounsDescriptorV2 } from '../../contracts/NounsDescriptorV2.sol';
+import { NounsSeeder } from '../../contracts/NounsSeeder.sol';
+import { IProxyRegistry } from '../../contracts/external/opensea/IProxyRegistry.sol';
+
+contract DescriptorUpgradeTest is Test, DescriptorHelpers {
+    NounsToken nounsToken;
+
+    function setUp() public {
+        IProxyRegistry proxyRegistry = IProxyRegistry(address(0));
+        address noundersDAO = address(1);
+        address minter = address(2);
+
+        NounsDescriptor descriptor = new NounsDescriptor();
+        _populateDescriptor(descriptor);
+        nounsToken = new NounsToken(noundersDAO, minter, descriptor, new NounsSeeder(), proxyRegistry);
+
+        vm.prank(minter);
+        nounsToken.mint();
+    }
+
+    function testUpgradeToV2MaintainsTokenURI() public {
+        string memory tokenURIwithV1 = nounsToken.tokenURI(1);
+
+        NounsDescriptorV2 descriptorV2 = _deployV2();
+        _populateDescriptorV2(descriptorV2);
+        nounsToken.setDescriptor(descriptorV2);
+
+        string memory tokenURIwithV2 = nounsToken.tokenURI(1);
+
+        // both _populateDescriptor and _populateDescriptorV2 are hard-coded to populate
+        // with the first item of each type found in image-data.json.
+        assertEq(tokenURIwithV2, tokenURIwithV1);
+    }
+}
---
+++ packages/nouns-contracts/test/foundry/helpers/Utils.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+
+abstract contract Utils {
+    // Convert an hexadecimal string to raw bytes
+    function fromHex(string memory s) internal pure returns (bytes memory) {
+        bytes memory ss = bytes(s);
+        require(ss.length % 2 == 0); // length must be even
+        bytes memory r = new bytes(ss.length / 2);
+        for (uint256 i = 0; i < ss.length / 2; ++i) {
+            r[i] = bytes1(fromHexChar(uint8(ss[2 * i])) * 16 + fromHexChar(uint8(ss[2 * i + 1])));
+        }
+        return r;
+    }
+
+    // Convert an hexadecimal character to their value
+    function fromHexChar(uint8 c) internal pure returns (uint8) {
+        if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
+            return c - uint8(bytes1('0'));
+        }
+        if (bytes1(c) >= bytes1('a') && bytes1(c) <= bytes1('f')) {
+            return 10 + c - uint8(bytes1('a'));
+        }
+        if (bytes1(c) >= bytes1('A') && bytes1(c) <= bytes1('F')) {
+            return 10 + c - uint8(bytes1('A'));
+        }
+        revert('fail');
+    }
+}
