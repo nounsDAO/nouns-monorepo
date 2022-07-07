0780d4e9f5088652a0b80bdefe639308cdcc2350
eladmallel <eladmallel@gmail.com>
2022-07-07 15:09:32 +0300

    only skipping the URI test when using coverage
    it works fine under regular testing
    it keeps failing under coverage due to timeouts and memory issues

--- packages/nouns-contracts/.solcover.js
+++ packages/nouns-contracts/.solcover.js
@@ -4,4 +4,8 @@ module.exports = {
     'test/WETH.sol',
   ],
   configureYulOptimizer: true,
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true               // Run the grep's inverse set.
+  }
 };
