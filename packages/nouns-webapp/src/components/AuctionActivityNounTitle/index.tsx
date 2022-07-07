1e9f6a2f122f4eda3a0fa199eaf161efe94f8107
meouner <bluewalrus33@protonmail.com>
2022-07-07 16:51:55 +0300

    fix meoun number

--- packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
@@ -7,7 +7,7 @@ const AuctionActivityNounTitle: React.FC<{ nounId: BigNumber; isCool?: boolean }
   return (
     <div className={classes.wrapper}>
       <h1 style={{ color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)' }}>
-        <Trans>Meoun {nounId.toString()}</Trans>
+        <Trans> Meoun {nounId.toString()}</Trans>
       </h1>
     </div>
   );
