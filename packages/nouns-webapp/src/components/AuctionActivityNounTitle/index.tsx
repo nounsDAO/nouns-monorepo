388bc7bc3bac7e9ae8dd26079bcc21993360d714
meouner <bluewalrus33@protonmail.com>
2022-07-07 17:09:12 +0300

    meoun 3

--- packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
@@ -3,11 +3,11 @@ import classes from './AuctionActivityNounTitle.module.css';
 import { Trans } from '@lingui/macro';
 
 const AuctionActivityNounTitle: React.FC<{ nounId: BigNumber; isCool?: boolean }> = props => {
-  const { nounId, isCool } = props;
+  const { meounId, isCool } = props;
   return (
     <div className={classes.wrapper}>
       <h1 style={{ color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)' }}>
-        <Trans>Meoun {nounId.toString()}</Trans>
+        <Trans>Meoun {meounId.toString()}</Trans>
       </h1>
     </div>
   );
