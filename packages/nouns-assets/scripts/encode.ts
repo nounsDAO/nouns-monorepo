23e5d1e6853322fb74be6791f372e7701904b110
David Brailovsky <davidbrai@gmail.com>
2022-07-11 18:46:29 +0200

    add task for encoding an additional json art file
    this also includes some testing utilities for quickly
    creating a bunch of art

--- packages/nouns-assets/scripts/encode.ts
+++ packages/nouns-assets/scripts/encode.ts
@@ -1,33 +1,9 @@
 import { PNGCollectionEncoder, PngImage } from '@nouns/sdk';
 import { promises as fs } from 'fs';
-import { PNG } from 'pngjs';
 import path from 'path';
+import { readPngImage } from './utils';
 
 const DESTINATION = path.join(__dirname, '../src/image-data_v2.json');
-
-/**
- * Read a PNG image file and return a `PngImage` object.
- * @param path The path to the PNG file
- */
-const readPngImage = async (path: string): Promise<PngImage> => {
-  const buffer = await fs.readFile(path);
-  const png = PNG.sync.read(buffer);
-
-  return {
-    width: png.width,
-    height: png.height,
-    rgbaAt: (x: number, y: number) => {
-      const idx = (png.width * y + x) << 2;
-      const [r, g, b, a] = [png.data[idx], png.data[idx + 1], png.data[idx + 2], png.data[idx + 3]];
-      return {
-        r,
-        g,
-        b,
-        a,
-      };
-    },
-  };
-};
 
 const encode = async () => {
   const encoder = new PNGCollectionEncoder();
