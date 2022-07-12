5e4ebd6eb7e130f5443da870ccc09de6b20b370b
eladmallel <eladmallel@gmail.com>
2022-07-12 16:53:39 +0300

    point webapp to latest subgraph

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -59,7 +59,7 @@ const app: Record<SupportedChains, AppConfig> = {
   [ChainId.Rinkeby]: {
     jsonRpcUri: createNetworkHttpUrl('rinkeby'),
     wsRpcUri: createNetworkWsUrl('rinkeby'),
-    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v4',
+    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/verbsteam/nouns-rinkeby',
     enableHistory: process.env.REACT_APP_ENABLE_HISTORY === 'true',
   },
   [ChainId.Mainnet]: {
