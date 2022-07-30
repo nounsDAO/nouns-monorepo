60142c8cd3cf4c6bcf7a586282ce2df7d1961849
pbrianandj <bcj26@case.edu>
2022-07-30 16:17:56 -0700

    make etherscan linking of delegate ens work

--- packages/nouns-webapp/src/components/profileEvent/eventData/ProposalVoteHeadline/index.tsx
+++ packages/nouns-webapp/src/components/profileEvent/eventData/ProposalVoteHeadline/index.tsx
@@ -1,6 +1,10 @@
+import classes from './ProposalVoteHeadline.module.css';
 import { Trans } from '@lingui/macro';
 import { Proposal, ProposalState, Vote } from '../../../../wrappers/nounsDao';
 import ShortAddress from '../../../ShortAddress';
+import { buildEtherscanAddressLink } from '../../../../utils/etherscan';
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 interface ProposalVoteHeadlineProps {
   proposal: Proposal;
@@ -19,9 +23,28 @@ const ProposalVoteHeadline: React.FC<ProposalVoteHeadlineProps> = props => {
   }
 
   const voterComponent = (
-    <span style={{ fontWeight: 'bold' }}>
-      <ShortAddress address={voter ?? ''} />
-    </span>
+    <>
+      <ReactTooltip
+        id={'view-on-etherscan-tooltip'}
+        effect={'solid'}
+        className={classes.delegateHover}
+        getContent={dataTip => {
+          return <div>{dataTip}</div>;
+        }}
+      />
+      <span
+        className={classes.voterLink}
+        data-tip={`View on Etherscan`}
+        data-for="view-on-etherscan-tooltip"
+        onClick={e => {
+          // This is so that we don't navigate to the prop page on click the address
+          e.stopPropagation();
+          window.open(buildEtherscanAddressLink(voter ?? ''), '_blank');
+        }}
+      >
+        <ShortAddress address={voter ?? ''} />
+      </span>
+    </>
   );
 
   switch (supportDetailed) {
