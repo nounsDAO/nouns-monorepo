8cb03238cec66c87b4af72b64ba115f1ede2aa51
Sidenoun <97852487+sidenoun@users.noreply.github.com>
2022-09-01 15:31:19 +0100

    fix quoted
    needs to be within quotes

--- packages/nouns-sdk/src/image/svg-builder.ts
+++ packages/nouns-sdk/src/image/svg-builder.ts
@@ -84,7 +84,7 @@ export const buildSVG = (
     });
     result += svgRects.join('');
     return result;
-  }, `<svg width=${width} height=${height} viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges"><rect width="100%" height="100%" fill="#${bgColor}" />`);
+  }, `<svg width="${width}" height="${height}" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges"><rect width="100%" height="100%" fill="#${bgColor}" />`);
 
   return `${svgWithoutEndTag}</svg>`;
 };
