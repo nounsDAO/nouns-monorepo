77668ade57e2201179647f8fb8dfeda8fc7dcdc2
ripe <unripeengine@gmail.com>
2022-09-01 12:24:28 -0700

    Create Explore page

---
+++ packages/nouns-webapp/src/pages/Explore/index.tsx
@@ -0,0 +1,15 @@
+import ExploreGrid from '../../components/ExploreGrid';
+
+interface ExplorePageProps {
+  
+}
+
+const ExplorePage: React.FC<ExplorePageProps> = props => {
+  
+  return (
+    <>
+        <ExploreGrid />
+    </>
+  );
+};
+export default ExplorePage;
