9e418edd13b187840a96d2de9b0e805e35209e46
Isaac Patka <ipatka@gmail.com>
2022-09-23 15:15:45 -0400

    remove macro

--- packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityNounTitle/index.tsx
@@ -1,6 +1,5 @@
 import { BigNumber } from 'ethers';
 import classes from './AuctionActivityNounTitle.module.css';
-import { Trans } from '@lingui/macro';
 
 const AuctionActivityNounTitle: React.FC<{ nounId: BigNumber; isCool?: boolean }> = props => {
   const { nounId, isCool } = props;
@@ -8,7 +7,8 @@ const AuctionActivityNounTitle: React.FC<{ nounId: BigNumber; isCool?: boolean }
   return (
     <div className={classes.wrapper}>
       <h1 style={{ color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)' }}>
-        <Trans>Public Noun {nounId.toString()}</Trans>
+        Public Noun {nounId.toString()}
+        {/* <Trans>Public Noun {nounId.toString()}</Trans> */}
       </h1>
     </div>
   );
