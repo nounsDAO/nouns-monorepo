b6d0e0a7a4b8bb72b4a4cc2814cc5cb85057df59
eladmallel <eladmallel@gmail.com>
2022-09-29 17:48:48 -0500

    deploy dao v2 logic: add etherscan verification

--- packages/nouns-contracts/tasks/deploy-v2-logic.ts
+++ packages/nouns-contracts/tasks/deploy-v2-logic.ts
@@ -10,7 +10,7 @@ promptjs.colors = false;
 promptjs.message = '> ';
 promptjs.delimiter = '';
 
-task('deploy-v2-logic', 'Deploys NounsDAOLogicV2').setAction(async (_args, { ethers }) => {
+task('deploy-v2-logic', 'Deploys NounsDAOLogicV2').setAction(async (_args, { ethers, run }) => {
   const factory = await ethers.getContractFactory('NounsDAOLogicV2');
   const signer = (await ethers.getSigners())[0];
 
@@ -25,6 +25,16 @@ task('deploy-v2-logic', 'Deploys NounsDAOLogicV2').setAction(async (_args, { eth
 
   console.log('Deploying...');
   const contract = await factory.deploy({ gasPrice });
+  await contract.deployed();
   console.log(`Transaction hash: ${contract.deployTransaction.hash} \n`);
   console.log(`NounsDAOLogicV2 deployed to ${contract.address}`);
+
+  await new Promise(f => setTimeout(f, 60000));
+
+  console.log('Verifying on Etherscan...');
+  await run('verify:verify', {
+    address: contract.address,
+    constructorArguments: [],
+  });
+  console.log('Verified');
 });
