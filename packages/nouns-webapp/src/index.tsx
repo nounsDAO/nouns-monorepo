acb14152c1adbd60cd3b4fa29969df12e34e7753
Isaac Patka <ipatka@gmail.com>
2022-09-29 05:06:40 -0400

    remove unused

--- packages/nouns-webapp/src/index.tsx
+++ packages/nouns-webapp/src/index.tsx
@@ -154,7 +154,6 @@ const ChainSubscriber: React.FC = () => {
     };
 
     // Fetch the current auction
-    const address = await nounsAuctionHouseContract.address
     const currentAuction = await nounsAuctionHouseContract.auction();
     dispatch(setFullAuction(reduxSafeAuction(currentAuction)));
     dispatch(setLastAuctionNounId(currentAuction.nounId.toNumber()));
