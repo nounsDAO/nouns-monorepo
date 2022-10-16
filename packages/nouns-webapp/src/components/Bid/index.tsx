8153aa3e12a7d6ae35093f781b98c61cadc60ac8
defnullx <defnullx@outlook.com>
2022-10-16 19:57:31 -0300

    Update Settle Manually Button

--- packages/nouns-webapp/src/components/Bid/index.tsx
+++ packages/nouns-webapp/src/components/Bid/index.tsx
@@ -291,17 +291,15 @@ const Bid: React.FC<{
           </Button>
         ) : (
           <>
-            <Col lg={12} className={classes.voteForNextNounBtnWrapper}>
-              <Button className={classes.bidBtnAuctionEnded} onClick={fomoNounsBtnOnClickHandler}>
-                <Trans>Vote for the next NounBR</Trans> ⌐◧-◧
-              </Button>
-            </Col>
             {/* Only show force settle button if wallet connected */}
             {isWalletConnected && (
-              <Col lg={12}>
-                <SettleManuallyBtn settleAuctionHandler={settleAuctionHandler} auction={auction} />
+              <Col lg={12} className={classes.voteForNextNounBtnWrapper}>
+                <Button className={classes.bidBtnAuctionEnded} onClick={settleAuctionHandler}>
+                  <Trans>Settle manually</Trans> ⌐◧-◧
+                </Button>
               </Col>
             )}
+              
           </>
         )}
       </InputGroup>
