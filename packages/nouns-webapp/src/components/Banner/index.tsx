3bc42e75897f6481ba95aca41cd5be7883886b88
defnullx <defnullx@outlook.com>
2022-10-19 19:09:35 -0300

    Update Carmen Miranda

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -1,7 +1,7 @@
 import classes from './Banner.module.css';
 import Section from '../../layout/Section';
 import { Col } from 'react-bootstrap';
-import calendar_noun from '../../assets/calendar_noun.png';
+import carmen_miranda from '../../assets/carmen_miranda.jpeg';
 import Noun from '../Noun';
 import { Trans } from '@lingui/macro';
 
@@ -21,7 +21,7 @@ const Banner = () => {
       </Col>
       <Col lg={6}>
         <div style={{ padding: '2rem' }}>
-          <Noun imgPath={calendar_noun} alt="nounBR" />
+          <Noun imgPath={carmen_miranda} alt="nounBR" />
         </div>
       </Col>
     </Section>
