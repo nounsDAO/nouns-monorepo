5402608d6931842aeefea894d54dba84561aa45c
defnullx <defnullx@outlook.com>
2022-10-27 17:33:14 -0300

    Update translate

--- packages/nouns-webapp/src/components/Documentation/index.tsx
+++ packages/nouns-webapp/src/components/Documentation/index.tsx
@@ -176,11 +176,7 @@ const Documentation = () => {
               </p>
               <p>
                 <Trans>
-                  The Nouns Foundation and NounsBR DAO considers the veto an emergency power that should not be
-                  exercised in the normal course of business. The NounsBR DAO will veto
-                  proposals that introduce non-trivial legal or existential risks to the NounsBR DAO
-                  , including (but not necessarily limited to) proposals
-                  that:
+                  The Nouns Foundation and NounsBR DAO considers the veto an emergency power that should not be exercised in the normal course of business. The NounsBR DAO will veto proposals that introduce non-trivial legal or existential risks to the NounsBR DAO , including (but not necessarily limited to) proposals that:
                 </Trans>
               </p>
               <ul>
