1184ec510add9611da218cd29cb9173429abd7a2
defnullx <defnullx@outlook.com>
2022-10-28 16:53:49 -0300

    Fix from the last commit

--- packages/nouns-webapp/src/utils/externalURL.ts
+++ packages/nouns-webapp/src/utils/externalURL.ts
@@ -1,4 +1,5 @@
 export enum ExternalURL {
+  discord,
   twitter,
   notion,
   about,
@@ -15,7 +16,7 @@ export const externalURL = (externalURL: ExternalURL) => {
     case ExternalURL.notion:
       return 'https://nouns.notion.site/Explore-NounsBR-a2a9dceeb1d54e10b9cbf3f931c2266f';
     case ExternalURL.about:
-        return 'https://nounsbr.com';
+       return 'https://nounsbr.com';
     case ExternalURL.discourse:
       return 'https://discourse.nounsbr.wtf/';
     case ExternalURL.nounsCenter:
