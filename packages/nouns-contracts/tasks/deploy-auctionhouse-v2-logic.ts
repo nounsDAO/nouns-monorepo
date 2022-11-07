06045cc3117c25479c87b7ebc94b8407495064e6
eladmallel <eladmallel@gmail.com>
2022-11-07 14:19:46 -0500

    update auction house v2 deploy
    to include the state migration contract

--- packages/nouns-contracts/tasks/deploy-auctionhouse-v2-logic.ts
+++ packages/nouns-contracts/tasks/deploy-auctionhouse-v2-logic.ts
@@ -12,30 +12,35 @@ promptjs.delimiter = '';
 
 task('deploy-auctionhouse-v2-logic', 'Deploys NounsAuctionHouseV2').setAction(
   async (_args, { ethers, run }) => {
-    const factory = await ethers.getContractFactory('NounsAuctionHouseV2');
-
     const gasPrice = await getGasPriceWithPrompt(ethers);
-    await printEstimatedCost(factory, gasPrice);
+    const contractNames = ['NounsAuctionHousePreV2Migration', 'NounsAuctionHouseV2'];
 
-    const deployConfirmed = await getDeploymentConfirmationWithPrompt();
-    if (!deployConfirmed) {
-      console.log('Exiting');
-      return;
+    for (const contractName of contractNames) {
+      const factory = await ethers.getContractFactory(contractName);
+
+      console.log(`Estimating deployment cost for ${contractName}...`);
+      await printEstimatedCost(factory, gasPrice);
+
+      const deployConfirmed = await getDeploymentConfirmationWithPrompt();
+      if (!deployConfirmed) {
+        console.log('Exiting');
+        return;
+      }
+
+      console.log('Deploying...');
+      const contract = await factory.deploy({ gasPrice });
+      await contract.deployed();
+      console.log(`Transaction hash: ${contract.deployTransaction.hash} \n`);
+      console.log(`${contractName} deployed to ${contract.address}`);
+
+      await new Promise(f => setTimeout(f, 60000));
+
+      console.log('Verifying on Etherscan...');
+      await run('verify:verify', {
+        address: contract.address,
+        constructorArguments: [],
+      });
+      console.log('Verified');
     }
-
-    console.log('Deploying...');
-    const contract = await factory.deploy({ gasPrice });
-    await contract.deployed();
-    console.log(`Transaction hash: ${contract.deployTransaction.hash} \n`);
-    console.log(`NounsAuctionHouseV2 deployed to ${contract.address}`);
-
-    await new Promise(f => setTimeout(f, 60000));
-
-    console.log('Verifying on Etherscan...');
-    await run('verify:verify', {
-      address: contract.address,
-      constructorArguments: [],
-    });
-    console.log('Verified');
   },
 );
