3c9ceedb956d2309a137c3b3633d2969fb3468a3
jack dishman <me@jackdishman.com>
2022-12-07 15:45:06 -0600

    feat: replace nouns assets with BDAO copy (#6)

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -4,6 +4,7 @@ import { Col } from 'react-bootstrap';
 import calendar_noun from '../../assets/calendar_noun.png';
 import Noun from '../Noun';
 import { Trans } from '@lingui/macro';
+import BostonDAOLogo from '../../components/BostonDAOLogo';
 
 const Banner = () => {
   return (
@@ -11,17 +12,19 @@ const Banner = () => {
       <Col lg={6}>
         <div className={classes.wrapper}>
           <h1>
-            <Trans>ONE NOUN,</Trans>
-            <br />
-            <Trans>EVERY DAY,</Trans>
-            <br />
-            <Trans>FOREVER.</Trans>
+            <Trans>
+              <blockquote>
+                “[The People] should never rise without doing something to be remembered – something
+                notable. And striking.”
+              </blockquote>
+              <br />- John Adams
+            </Trans>
           </h1>
         </div>
       </Col>
       <Col lg={6}>
         <div style={{ padding: '2rem' }}>
-          <Noun imgPath={calendar_noun} alt="noun" />
+          <BostonDAOLogo />
         </div>
       </Col>
     </Section>
