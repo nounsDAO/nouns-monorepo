c2880306f90c9f6ad6c3c66e3351ba19e1a73fff
n00uns <121721625+n00uns@users.noreply.github.com>
2023-01-11 09:32:36 -0500

    fix build error for links

--- packages/n00uns-webapp/src/pages/index.tsx
+++ packages/n00uns-webapp/src/pages/index.tsx
@@ -14,6 +14,7 @@ const Landing: NextPage = (props) => {
           src={logo}
           className="mx-auto object-cover w-72 mb-48"
           alt="n00uns logo"
+          aria-label="n00uns logo"
         />
         <div className="w-[100%] mx-auto absolute overflow-hidden">
           <Player
@@ -33,13 +34,19 @@ const Landing: NextPage = (props) => {
           <ul className="flex flex-wrap items-center mx-auto mt-5 text-gray-500 space-x-4">
               <li>
                   <a href="https://twitter.com/snounsdao?s=20&t=YOW-qmN9O20RX-lB-fOmiA" 
-                  target="_blank"
-                  className="mr-4 hover:scale-110 md:mr-6 ">
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="link to n00uns twitter"
+                    className="mr-4 hover:scale-110 md:mr-6 ">
                     <FaTwitter className="text-3xl" />
                   </a>
               </li>
               <li>
-                  <a href="discord.gg/2Gp55Uzsad" target="_blank" className="mr-4 hover:scale-110 md:mr-6">
+                  <a href="discord.gg/2Gp55Uzsad" 
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="link to n00uns discord"
+                    className="mr-4 hover:scale-110 md:mr-6">
                     <FaDiscord className="text-3xl" />
                   </a>
               </li>
