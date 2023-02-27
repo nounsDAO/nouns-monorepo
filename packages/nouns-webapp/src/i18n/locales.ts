c3e2555647be89ac44022f6909269f4cfc0f6631
fiveoceans-dev <niip42@gmail.com>
2023-02-28 04:13:50 +0900

    add korean language support

--- packages/nouns-webapp/src/i18n/locales.ts
+++ packages/nouns-webapp/src/i18n/locales.ts
@@ -1,10 +1,12 @@
 import en, { Locale as DaysJSLocale } from 'dayjs/locale/en';
 import ja from 'dayjs/locale/ja';
+import ko from 'dayjs/locale/ko';
 
 export const SUPPORTED_LOCALES = [
   // order as they appear in the language dropdown
   'en-US',
   'ja-JP',
+  'ko-KOR',
 ];
 export type SupportedLocale = typeof SUPPORTED_LOCALES[number] | 'pseudo';
 
@@ -13,17 +15,20 @@ export const DEFAULT_LOCALE: SupportedLocale = 'en-US';
 export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
   'en-US': 'English',
   'ja-JP': '日本語',
+  'ko-KOR': '한국어',
   pseudo: 'ƥƨèúδô',
 };
 
 export enum Locales {
   en_US = 'en-US',
   ja_JP = 'ja-JP',
+  kor_KOR = 'ko-KOR',  
 }
 
 // Map SupportedLocale string to DaysJS locale object (used for locale aware time formatting)
 export const SUPPORTED_LOCALE_TO_DAYSJS_LOCALE: { [locale in SupportedLocale]: DaysJSLocale } = {
   'en-US': en,
   'ja-JP': ja,
+  'ko-KOR': ko,  
   pseudo: en,
 };
