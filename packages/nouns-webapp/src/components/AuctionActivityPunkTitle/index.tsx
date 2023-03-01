88c724b49068769e80a7d0e0bd8c80853fc9f1fc
natkins-dev <natkins.dev@gmail.com>
2023-03-01 06:07:26 +0100

    fix: problematic translation removed

--- packages/nouns-webapp/src/components/AuctionActivityPunkTitle/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityPunkTitle/index.tsx
@@ -1,13 +1,12 @@
 import { BigNumber } from 'ethers';
 import classes from './AuctionActivityPunkTitle.module.css';
-import { Trans } from '@lingui/macro';
 
 const AuctionActivityPunkTitle: React.FC<{ tokenId: BigNumber; isCool?: boolean }> = props => {
   const { tokenId, isCool } = props;
   return (
     <div className={classes.wrapper}>
       <h1 style={{ color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)' }}>
-        <Trans>Punk {tokenId.toString()}</Trans>
+        Punk {tokenId.toString()}
       </h1>
     </div>
   );
