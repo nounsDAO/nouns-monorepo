482f1cee7dfd7fe80ddf8b2bb23fcc07050e8464
natkins-dev <natkins.dev@gmail.com>
2023-03-01 06:31:13 +0100

    fix: spinner as loader

--- packages/nouns-webapp/src/components/Punk/index.tsx
+++ packages/nouns-webapp/src/components/Punk/index.tsx
@@ -1,11 +1,14 @@
 import classes from './Token.module.css';
 import React from 'react';
-import loadingPunk from '../../assets/loading-skull-noun.gif';
+// import loadingPunk from '../../assets/loading-skull-noun.gif';
 import Image from 'react-bootstrap/Image';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const LoadingPunk = () => {
   return (
     <div className={classes.imgWrapper}>
+      <FontAwesomeIcon icon={faSpinner} spin className={classes.spinner} />
       {/*<Image className={classes.img} src={loadingPunk} alt={'loading punk'} fluid />*/}
     </div>
   );
@@ -20,12 +23,11 @@ const Punk: React.FC<{
   const { imgPath, alt, className, wrapperClassName } = props;
   return (
     <div className={`${classes.imgWrapper} ${wrapperClassName}`}>
-      <Image
-        className={`${classes.img} ${className}`}
-        src={imgPath ? imgPath : loadingPunk}
-        alt={alt}
-        fluid
-      />
+      {imgPath ? (
+        <Image className={`${classes.img} ${className}`} src={imgPath} alt={alt} fluid />
+      ) : (
+        <FontAwesomeIcon icon={faSpinner} spin className={classes.spinner} />
+      )}
     </div>
   );
 };
