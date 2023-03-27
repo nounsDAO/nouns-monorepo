e494f906a4e71e7dc69fecc5d17efb90eeb70530
solimander <solimander-dev@protonmail.com>
2023-03-27 11:45:41 -0600

    Link to hackweek.wtf

--- packages/nouns-webapp/src/components/HackathonBanner/index.tsx
+++ packages/nouns-webapp/src/components/HackathonBanner/index.tsx
@@ -3,7 +3,7 @@ import classes from './HackathonBanner.module.css';
 const HackathonBanner: React.FC<{}> = () => {
   return (
     <div className={classes.banner}>
-      <a href="https://twitter.com/nounsprophouse/status/1640352924848078849" target="_blank" rel="noreferrer">
+      <a href="https://hackweek.wtf/" target="_blank" rel="noreferrer">
         Hack Week is here! Nouns is giving 99 ETH in prizes to builders and creators of all
         backgrounds. <b>Make something Nounish this week →</b>
       </a>
