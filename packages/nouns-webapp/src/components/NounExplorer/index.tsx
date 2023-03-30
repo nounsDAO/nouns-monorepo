bd49340ff89ca7df5f0b0633c15a581a1fd6f072
devcarrot <devcarrot@protonmail.com>
2023-03-29 23:13:55 -0600

    Add clouds

--- packages/nouns-webapp/src/components/NounExplorer/index.tsx
+++ packages/nouns-webapp/src/components/NounExplorer/index.tsx
@@ -4,10 +4,14 @@ import { BigNumber } from 'ethers';
 import classes from './NounExplorer.module.css';
 import CarouselItem from '../CarouselItem';
 import { StandaloneNounImage } from '../StandaloneNoun';
+import Cloud from '../Cloud';
+import { range } from 'ramda';
 
 interface NounExplorerProps {
   auction?: Auction;
 }
+
+const random = (min: number, max: number) => Math.random() * (max - min) + min;
 
 const NounExplorer: React.FC<NounExplorerProps> = props => {
   // TODO: Load next 50 nouns when user reaches the end of the carousel
@@ -19,6 +23,15 @@ const NounExplorer: React.FC<NounExplorerProps> = props => {
 
   return (
     <div className={classes.explorer}>
+      {/* TODO: use different size clouds rather than scaling */}
+      {range(0, 5).map(i => (
+        <Cloud
+          key={i}
+          offset={random(0, 100)}
+          size={random(125, 200)}
+          startOffset={random(0, 65)}
+        />
+      ))}
       <Carousel
         rootClassName={classes.carousel}
         scrollClassName={classes.scroll}
