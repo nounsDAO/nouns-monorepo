a968b13db9991501b22692e52cb56babd6c3026f
lukasz-glen <unknown>
2023-04-18 16:06:58 +0200

    fix: calculating hashes for OG punks

--- packages/nouns-contracts/tasks/utils/index.ts
+++ packages/nouns-contracts/tasks/utils/index.ts
@@ -38,3 +38,50 @@ export function printContractsTable(contracts: Record<ContractName, DeployedCont
     ),
   );
 }
+
+export interface Acc {
+  accType: number;
+  accId: number;
+}
+
+export interface Seed {
+  punkType: number;
+  skinTone: number;
+  accessories: Array<Acc>;
+}
+
+// this function must be the same as test/nouns.test.ts/calculateSeedHash()
+// the above function is tested against contract logic
+// if changed please update here
+export function calculateSeedHash(seed: Seed) {
+  const data = [];
+
+  if (seed.punkType > 255) {
+    throw new Error('Invalid value');
+  }
+  data.push(seed.punkType);
+  if (seed.skinTone > 255) {
+    throw new Error('Invalid value');
+  }
+  data.push(seed.skinTone);
+  if (seed.accessories.length > 14) {
+    throw new Error('Invalid value');
+  }
+  data.push(seed.accessories.length);
+  seed.accessories.sort((acc1, acc2) => acc1.accType - acc2.accType);
+  let prevAccType = -1; // check if acc types repeat
+  seed.accessories.forEach( acc => {
+    if (acc.accType > 255 || acc.accId > 255 || acc.accType == prevAccType) {
+      throw new Error('Invalid value');
+    }
+    prevAccType = acc.accType;
+    data.push(acc.accType);
+    data.push(acc.accId);
+  });
+
+  let seedHash = '';
+  data.forEach(n => { seedHash = (n > 15 ? '' : '0') + n.toString(16) + seedHash; });
+  seedHash = '0x' + seedHash.padStart(64, '0');
+
+  return seedHash;
+}
