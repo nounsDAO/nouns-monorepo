f6384a1b238030fd6c34c027b5429e7d6956e627
David Brailovsky <davidbrai@gmail.com>
2023-05-08 14:01:15 +0200

    fix test

--- packages/nouns-contracts/test/foundry/helpers/ForkDAODeployerMock.sol
+++ packages/nouns-contracts/test/foundry/helpers/ForkDAODeployerMock.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.15;
 import 'forge-std/Test.sol';
 import { IForkDAODeployer } from '../../../contracts/governance/NounsDAOInterfaces.sol';
 
+contract ForkDAOTokenMock {
+    function claimDuringForkPeriod(address to, uint256[] calldata tokenIds) external {}
+}
+
 contract ForkDAODeployerMock is IForkDAODeployer, StdCheats {
     address public mockTreasury = makeAddr('mock treasury');
-    address public mockToken = makeAddr('mock token');
+    address public mockToken = address(new ForkDAOTokenMock());
 
     function deployForkDAO() external view returns (address treasury, address token) {
         treasury = mockTreasury;
