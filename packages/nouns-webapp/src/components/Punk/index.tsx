cc4fd0c6e1c8b6a1775d3018d93f4662fb18a95c
natkins-dev <natkins.dev@gmail.com>
2023-05-11 20:14:37 +0200

    feat: loader

--- packages/nouns-webapp/src/components/Punk/index.tsx
+++ packages/nouns-webapp/src/components/Punk/index.tsx
@@ -1,33 +1,33 @@
 import classes from './Token.module.css';
 import React from 'react';
-// import loadingPunk from '../../assets/loading-skull-noun.gif';
+import loadingPunk from '../../assets/loading-punk.gif';
 import Image from 'react-bootstrap/Image';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const LoadingPunk = () => {
   return (
     <div className={classes.imgWrapper}>
-      <FontAwesomeIcon icon={faSpinner} spin className={classes.spinner} />
-      {/*<Image className={classes.img} src={loadingPunk} alt={'loading punk'} fluid />*/}
+      <Image className={classes.img} src={loadingPunk} alt={'loading punk'} fluid />
     </div>
   );
 };
 
-const Punk: React.FC<{
+interface PunkProps {
   imgPath: string;
   alt: string;
   className?: string;
   wrapperClassName?: string;
-}> = props => {
+}
+
+const Punk: React.FC<PunkProps> = props => {
   const { imgPath, alt, className, wrapperClassName } = props;
   return (
     <div className={`${classes.imgWrapper} ${wrapperClassName}`}>
-      {imgPath ? (
-        <Image className={`${classes.img} ${className}`} src={imgPath} alt={alt} fluid />
-      ) : (
-        <FontAwesomeIcon icon={faSpinner} spin className={classes.spinner} />
-      )}
+      <Image
+        className={`${classes.img} ${className}`}
+        src={imgPath ? imgPath : loadingPunk}
+        alt={alt}
+        fluid
+      />
     </div>
   );
 };
