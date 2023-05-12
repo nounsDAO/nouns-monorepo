974f9f477241baf02b56c9bd1fe56d18e500d6c2
natkins-dev <natkins.dev@gmail.com>
2023-05-12 08:19:54 +0200

    feat: footer

--- packages/nouns-webapp/src/components/Footer/index.tsx
+++ packages/nouns-webapp/src/components/Footer/index.tsx
@@ -1,14 +1,14 @@
 import classes from './Footer.module.css';
 import { Container } from 'react-bootstrap';
 import { buildEtherscanAddressLink } from '../../utils/etherscan';
-// import { externalURL, ExternalURL } from '../../utils/externalURL';
+import { externalURL, ExternalURL } from '../../utils/externalURL';
 import config from '../../config';
 import Link from '../Link';
 import { Trans } from '@lingui/macro';
 
 const Footer = () => {
-  // const twitterURL = externalURL(ExternalURL.twitter);
-  // const discordURL = externalURL(ExternalURL.discord);
+  const twitterURL = externalURL(ExternalURL.twitter);
+  const discordURL = externalURL(ExternalURL.discord);
   const etherscanURL = buildEtherscanAddressLink(config.addresses.nToken);
   // const discourseURL = externalURL(ExternalURL.discourse);
 
@@ -16,8 +16,8 @@ const Footer = () => {
     <div className={classes.wrapper}>
       <Container className={classes.container}>
         <footer className={classes.footerSignature}>
-          {/*<Link text={<Trans>Discord</Trans>} url={discordURL} leavesPage={true} />*/}
-          {/*<Link text={<Trans>Twitter</Trans>} url={twitterURL} leavesPage={true} />*/}
+          <Link text={<Trans>Discord</Trans>} url={discordURL} leavesPage={true} />
+          <Link text={<Trans>Twitter</Trans>} url={twitterURL} leavesPage={true} />
           <Link text={<Trans>Etherscan</Trans>} url={etherscanURL} leavesPage={true} />
           {/*<Link text={<Trans>Forums</Trans>} url={discourseURL} leavesPage={false} />*/}
         </footer>
