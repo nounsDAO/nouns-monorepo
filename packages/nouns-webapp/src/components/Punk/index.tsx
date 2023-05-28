3ef92cae0443c4cd48369e89d81f49fb9e2b2868
natkins-dev <natkins.dev@gmail.com>
2023-05-28 14:07:18 +0200

    fix: loader

--- packages/nouns-webapp/src/components/Punk/index.tsx
+++ packages/nouns-webapp/src/components/Punk/index.tsx
@@ -1,12 +1,12 @@
 import classes from './Token.module.css';
 import React from 'react';
-import loadingPunk from '../../assets/loading-punk.gif';
+import alienPunkLoader from '../../assets/alien-punk-loader.gif';
 import Image from 'react-bootstrap/Image';
 
 export const LoadingPunk = () => {
   return (
-    <div className={classes.imgWrapper}>
-      <Image className={classes.img} src={loadingPunk} alt={'loading punk'} fluid />
+    <div className={classes.loaderWrapper}>
+      <Image className={classes.loader} src={alienPunkLoader} alt="loading punk" />
     </div>
   );
 };
@@ -20,15 +20,12 @@ interface PunkProps {
 
 const Punk: React.FC<PunkProps> = props => {
   const { imgPath, alt, className, wrapperClassName } = props;
-  return (
+  return imgPath.length ? (
     <div className={`${classes.imgWrapper} ${wrapperClassName}`}>
-      <Image
-        className={`${classes.img} ${className}`}
-        src={imgPath ? imgPath : loadingPunk}
-        alt={alt}
-        fluid
-      />
+      <Image className={`${classes.img} ${className}`} src={imgPath} alt={alt} fluid />
     </div>
+  ) : (
+    <LoadingPunk />
   );
 };
 
