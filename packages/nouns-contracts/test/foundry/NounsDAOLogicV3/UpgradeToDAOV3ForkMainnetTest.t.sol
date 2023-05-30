5f5e58f5875c63ffb4dcf4040264dfa556b4a95f
eladmallel <eladmallel@gmail.com>
2023-05-30 13:58:05 -0500

    add script to propose timelock v1 cleanup

--- packages/nouns-contracts/test/foundry/NounsDAOLogicV3/UpgradeToDAOV3ForkMainnetTest.t.sol
+++ packages/nouns-contracts/test/foundry/NounsDAOLogicV3/UpgradeToDAOV3ForkMainnetTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.15;
 import 'forge-std/Test.sol';
 import { ProposeDAOV3UpgradeMainnet } from '../../../script/ProposeDAOV3UpgradeMainnet.s.sol';
 import { DeployDAOV3NewContractsMainnet } from '../../../script/DeployDAOV3NewContractsMainnet.s.sol';
+import { ProposeTimelockMigrationCleanupMainnet } from '../../../script/ProposeTimelockMigrationCleanupMainnet.s.sol';
 import { NounsDAOLogicV1 } from '../../../contracts/governance/NounsDAOLogicV1.sol';
 import { NounsDAOLogicV3 } from '../../../contracts/governance/NounsDAOLogicV3.sol';
 import { NounsDAOProxy } from '../../../contracts/governance/NounsDAOProxy.sol';
@@ -22,6 +23,8 @@ import { NounsTokenFork } from '../../../contracts/governance/fork/newdao/token/
 import { NounsDAOLogicV1Fork } from '../../../contracts/governance/fork/newdao/governance/NounsDAOLogicV1Fork.sol';
 import { ENSNamehash } from '../lib/ENSNamehash.sol';
 import '../lib/ENSInterfaces.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface IHasName {
     function NAME() external pure returns (string memory);
@@ -40,6 +43,9 @@ contract UpgradeToDAOV3ForkMainnetTest is Test {
         NounsDAOLogicV1(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d);
     INounsDAOExecutor public constant NOUNS_TIMELOCK_V1_MAINNET =
         INounsDAOExecutor(0x0BC3807Ec262cB779b38D65b38158acC3bfedE10);
+    address public constant AUCTION_HOUSE_PROXY_ADMIN_MAINNET = 0xC1C119932d78aB9080862C5fcb964029f086401e;
+    address public constant DESCRIPTOR_MAINNET = 0x6229c811D04501523C6058bfAAc29c91bb586268;
+    address public constant LILNOUNS_MAINNET = 0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B;
     address whaleAddr = 0xf6B6F07862A02C85628B3A9688beae07fEA9C863;
     uint256 public constant INITIAL_ETH_IN_TREASURY = 12919915363316446110962;
     uint256 public constant STETH_BALANCE = 14931432047776533741220;
@@ -236,6 +242,19 @@ contract UpgradeToDAOV3ForkMainnetTest is Test {
         assertEq(forkDao.votingPeriod(), 36000);
         assertEq(forkDao.proposalThresholdBPS(), 25);
         assertEq(forkDao.quorumVotesBPS(), 1000);
+    }
+
+    function test_timelockV1CleanupProposal() public {
+        assertGt(address(NOUNS_TIMELOCK_V1_MAINNET).balance, 2919 ether);
+
+        proposalId = new ProposeTimelockMigrationCleanupMainnet().run();
+        executeUpgradeProposal();
+
+        assertEq(nouns.owner(), address(timelockV2));
+        assertEq(Ownable(DESCRIPTOR_MAINNET).owner(), address(timelockV2));
+        assertEq(Ownable(AUCTION_HOUSE_PROXY_ADMIN_MAINNET).owner(), address(timelockV2));
+        assertEq(address(NOUNS_TIMELOCK_V1_MAINNET).balance, 0);
+        assertTrue(IERC721(LILNOUNS_MAINNET).isApprovedForAll(address(NOUNS_TIMELOCK_V1_MAINNET), address(timelockV2)));
     }
 
     function test_ensChange_nounsDotETHResolvesBothWaysWithTimelockV2() public {
