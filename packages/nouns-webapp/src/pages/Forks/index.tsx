2f3b00ad9d9b5e0a2757444514ed6f230b5a1c4f
ripe <unripeengine@gmail.com>
2023-06-28 11:48:08 -0700

    forks page header styling

--- packages/nouns-webapp/src/pages/Forks/index.tsx
+++ packages/nouns-webapp/src/pages/Forks/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Fork, useForks } from '../../wrappers/nounsDao';
 import { Link } from 'react-router-dom';
+import Section from '../../layout/Section';
+import { Col, Row } from 'react-bootstrap';
+import { Trans } from '@lingui/macro';
+import classes from './Forks.module.css';
 
 type Props = {}
 
@@ -14,27 +18,55 @@ const ForksPage: React.FC<Props> = props => {
   const nextForkId = forks.data && forks.data.length;
   return (
     <div>
-      {/* if latest fork id is finished forking, display a callout with an option to start a new fork. */}
-
-      {forks.data && forks.data.map((fork: Fork, i: number) => {
-        return (
-          <div key={i}>
-            <Link to={`/fork/${fork.id}`}>
-              {fork.id}
-            </Link>
-          </div>
-        )
-      })}
-      {isLatestForkFinished && nextForkId && (
-        <div>
-          <p>
-            The latest fork is finished forking. Start a new fork.
+      <Section fullWidth={false} className={classes.section}>
+        <Col lg={10} className={classes.wrapper}>
+          <Row className={classes.headerRow}>
+            <span>
+              <Trans>Governance</Trans>
+            </span>
+            <h1>
+              <Trans>Forks</Trans>
+            </h1>
+          </Row>
+          <p className={classes.subheading}>
+            <Trans>
+              TKTKTK Nouns govern <span className={classes.boldText}>Nouns DAO</span>. Nouns can vote on
+              proposals or delegate their vote to a third party.
+            </Trans>
           </p>
-          <Link to={`/fork/${nextForkId}`}>
-            Start a new fork
-          </Link>
-        </div>
-      )}
+
+
+        </Col>
+      </Section>
+      {/* if latest fork id is finished forking, display a callout with an option to start a new fork. */}
+      <Section fullWidth={false} className={classes.section}>
+        <Col lg={10} className={classes.wrapper}>
+          <Row className={classes.forksList}>
+
+            {forks.data && forks.data.map((fork: Fork, i: number) => {
+              return (
+                <div key={i}>
+                  <Link to={`/fork/${fork.id}`}>
+                    {fork.id}
+                  </Link>
+                </div>
+              )
+            })}
+          </Row>
+          <Row>
+            {isLatestForkFinished && nextForkId && (
+              <div>
+                <p>
+                  The latest fork is finished forking. Start a new fork.
+                </p>
+                <Link to={`/fork/${nextForkId}`}>
+                  Start a new fork
+                </Link>
+              </div>
+            )}
+          </Row>
+        </Col>
+      </Section>
     </div>
   )
 }
