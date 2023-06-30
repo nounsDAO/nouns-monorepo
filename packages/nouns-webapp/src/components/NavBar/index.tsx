8dae405c812db8b9225294690b2aea3973c89035
Jesse Paterson <jesse.r.pat@gmail.com>
2023-06-29 19:27:28 -0500

    Updated Logo

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from '../../hooks';
 import classes from './NavBar.module.css';
-import noggles from '../../assets/noggles.svg';
+import logo from '../../assets/logo.png';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap';
@@ -62,7 +62,7 @@ const NavBar = () => {
         <Container style={{ maxWidth: 'unset' }}>
           <div className={classes.brandAndTreasuryWrapper}>
             <Navbar.Brand as={Link} to="/" className={classes.navBarBrand}>
-              <img src={noggles} className={classes.navBarLogo} alt="Nouns DAO noggles" />
+              <img src={logo} className={classes.navBarLogo} alt="ATX DAO Logo" />
             </Navbar.Brand>
             {Number(CHAIN_ID) !== 1 && (
               <Nav.Item>
