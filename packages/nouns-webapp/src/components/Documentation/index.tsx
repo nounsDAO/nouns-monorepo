2e982310705316a71a652d49c52f44b3c3020642
Jesse Paterson <jesse.r.pat@gmail.com>
2023-07-24 20:15:46 -0500

    updated dates

--- packages/nouns-webapp/src/components/Documentation/index.tsx
+++ packages/nouns-webapp/src/components/Documentation/index.tsx
@@ -63,10 +63,10 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
               <Trans>Who we are</Trans>
             </Accordion.Header>
             <Accordion.Body>
-            <p>ATX DAO is an organization of members passionate about the future of technology, creativity, 
-               and governance united by a shared love for the city of Austin. We are founders, artists, traders, 
-               thought leaders, professionals, engineers, and investors working together to make Austin a leading city 
-               in the crypto industry and a better city as a whole. We are a group of friends and collaborators with a shared 
+            <p>ATX DAO is an organization of members passionate about the future of technology, creativity,
+               and governance united by a shared love for the city of Austin. We are founders, artists, traders,
+               thought leaders, professionals, engineers, and investors working together to make Austin a leading city
+               in the crypto industry and a better city as a whole. We are a group of friends and collaborators with a shared
                treasury and ideas about how to use it.</p>
 
             {/* <a href="https://www.youtube.com/watch?v=ZvC-WN10E5o" target="#">A New Austin Institution</a>. */}
@@ -76,7 +76,7 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
             <br/>
             <p>ATX DAO was founded in October 2021 as a social organization and has grown into a new Austin institution.
               Due to the combined efforts of the membership base, ATX DAO has established itself as a leading City DAO,
-              responsible for accomplishing tasks many would have deemed impossible from a group of people operating in an organic, 
+              responsible for accomplishing tasks many would have deemed impossible from a group of people operating in an organic,
               decentralized, and grassroots manner.</p>
 
             {/* <a href="https://www.youtube.com/watch?v=NUh8UGEXjJ4" target="#">Introducing ATX DAO</a>. */}
@@ -85,9 +85,9 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
             <br/>
             <br/>
 <p>As a DAO, each member is able to <a href="https://snapshot.org/#/atxdao.eth" target="#">create a proposal and vote on the future of our organization</a>.
-    Membership is signified by an NFT and 1 NFT = 1 vote. Anyone with an NFT can create a proposal 
-    about what the organization should do and what the treasury should be spent on. 
-    This makes ATX DAO the most democratic institution in the city, and our bottom-up, self-governing system is an 
+    Membership is signified by an NFT and 1 NFT = 1 vote. Anyone with an NFT can create a proposal
+    about what the organization should do and what the treasury should be spent on.
+    This makes ATX DAO the most democratic institution in the city, and our bottom-up, self-governing system is an
     innovative structure that puts us in a position to have an impact in ways never before seen in Austin.
    </p>
             </Accordion.Body>
@@ -106,7 +106,7 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
             </Accordion.Body>
           </Accordion.Item>
 
-         
+
 
           <Accordion.Item eventKey="2" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
@@ -114,7 +114,7 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
             </Accordion.Header>
             <Accordion.Body>
               <Trans>
-              ATX DAO members are not required to contribute. The simple act of being a part of the network and minting an NFT brings immense value to the DAO. However, it is encouraged that you take advantage of the events we organize to build relationships and enjoy yourselves. If you are interested in being a leader or a contributor, please do not hesitate to vocalize that desire to a fellow member, DAO Operator (Mason Lynaugh or Megan Murray), or a Guild Guide. 
+              ATX DAO members are not required to contribute. The simple act of being a part of the network and minting an NFT brings immense value to the DAO. However, it is encouraged that you take advantage of the events we organize to build relationships and enjoy yourselves. If you are interested in being a leader or a contributor, please do not hesitate to vocalize that desire to a fellow member, DAO Operator (Mason Lynaugh or Megan Murray), or a Guild Guide.
               </Trans>
             </Accordion.Body>
           </Accordion.Item>
@@ -126,9 +126,9 @@ const Documentation = (props: DocumentationProps = { backgroundColor: '#FFF' }) 
               <Trans>
               <b>Subscribe to the <a href="https://calendar.google.com/calendar/u/0/r?cid=c_k4h21h5hj127usvnf4lgvbkvd8@group.calendar.google.com" target="#">events calendar</a> to stay informed about ATX DAO events.</b>
               <br/>
-              <b>- Weekly</b> <br/>Every Monday at 5:30 PM we have an open floor conversation in Discord Voice. Every Wednesday at 8:00 PM is the weekly Engineering meeting in Discord Voice. 
+              <b>- Weekly</b> <br/>Every Monday at 5:30 PM we have an open floor conversation in Discord Voice. Every Wednesday at 8:00 PM is the weekly Engineering meeting in Discord Voice.
               <br/>
-              <b>- Monthly</b> <br/> On the first Tuesday or Wednesday of every month, we meet at Keiretsu from 6-8 PM to socialize, strategize, and reward contributions via the giving circle. Pizza and beverages are provided. On the final Thursday of the month, we host a public hacker meetup. 
+              <b>- Monthly</b> <br/> On the second Tuesday of every month, we meet at Keiretsu from 6-8 PM to socialize, strategize, and reward contributions via the giving circle. Pizza and beverages are provided. On the final Thursday of the month, we host a public hacker meetup. 
               <br/>
               <b>- Yearly</b> <br/> ATX DAO hosts an annual gala toward the end of the year for DAO members, plus ones, sponsors / strategic partners as needed. The dress code is formal. Tickets are sold to the general public at a high price point. ATX DAO also hosts an annual Web3 Pitch Competition.
               <br/>
