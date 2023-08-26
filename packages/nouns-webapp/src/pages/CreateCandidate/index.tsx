1ceb1fbcf1649edaa7344a8bf947d1a270d8ee00
solimander <solimander-dev@protonmail.com>
2023-08-26 10:09:27 -0600

    Only show top up if paying in USDC

--- packages/nouns-webapp/src/pages/CreateCandidate/index.tsx
+++ packages/nouns-webapp/src/pages/CreateCandidate/index.tsx
@@ -66,7 +66,7 @@ const CreateCandidatePage = () => {
   );
 
   useEffect(() => {
-    if (ethNeeded !== undefined && ethNeeded !== tokenBuyerTopUpEth) {
+    if (ethNeeded !== undefined && ethNeeded !== tokenBuyerTopUpEth && totalUSDCPayment > 0) {
       const hasTokenBuyterTopTop =
         proposalTransactions.filter(txn => txn.address === config.addresses.tokenBuyer).length > 0;
 
