cd2d8f68fb632975b363bc88971f9c57cb02269b
davidbrai <davidbrai@gmail.com>
2023-10-06 12:40:54 +0000

    cleanup

--- packages/nouns-contracts/script/DeployAuctionHouseV2Sepolia.s.sol
+++ packages/nouns-contracts/script/DeployAuctionHouseV2Sepolia.s.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 
-import 'forge-std/Script.sol';
-import { NounsAuctionHouse } from '../contracts/NounsAuctionHouse.sol';
-import { NounsAuctionHouseV2 } from '../contracts/NounsAuctionHouseV2.sol';
-import { NounsAuctionHousePreV2Migration } from '../contracts/NounsAuctionHousePreV2Migration.sol';
-
 import { DeployAuctionHouseV2Base } from './DeployAuctionHouseV2Base.s.sol';
 
 contract DeployAuctionHouseV2Sepolia is DeployAuctionHouseV2Base {
