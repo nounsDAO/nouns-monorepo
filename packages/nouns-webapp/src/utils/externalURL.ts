89d9c1c70425481a2a5e0455c1964d9d7ee7e028
Shigeki TORII <torichanjp@gmail.com>
2024-02-08 12:40:45 +0900

    Update footer links for UY etc
    * Twitter
    * Forums

--- packages/nouns-webapp/src/utils/externalURL.ts
+++ packages/nouns-webapp/src/utils/externalURL.ts
@@ -8,11 +8,11 @@ export enum ExternalURL {
 export const externalURL = (externalURL: ExternalURL) => {
   switch (externalURL) {
     case ExternalURL.twitter:
-      return 'https://twitter.com/nounsdao';
+      return 'https://twitter.com/ubuyamaNounsDAO';
     case ExternalURL.nounsProtocol:
       return 'https://nouns.notion.site/Explore-Nouns-a2a9dceeb1d54e10b9cbf3f931c2266f';
     case ExternalURL.discourse:
-      return 'https://discourse.nouns.wtf/';
+      return 'https://discord.gg/hZ3MNmYdC4';
     case ExternalURL.document:
       return 'https://ubuyamadao.notion.site/DAO-bdab4020b66f404696c855e86c57bdec?pvs=4';
   }
