00fdc3c800b75e48703533a8ac1d321019b5a2be
eladmallel <eladmallel@gmail.com>
2024-03-01 09:14:41 -0500

    client token: update NFT art

--- packages/nouns-contracts/contracts/client-incentives/NounsClientTokenDescriptor.sol
+++ packages/nouns-contracts/contracts/client-incentives/NounsClientTokenDescriptor.sol
@@ -21,19 +21,22 @@ import { INounsClientTokenDescriptor } from './INounsClientTokenDescriptor.sol';
 import { INounsClientTokenTypes } from './INounsClientTokenTypes.sol';
 import { Base64 } from 'base64-sol/base64.sol';
 import { Strings } from '@openzeppelin/contracts/utils/Strings.sol';
+import { ETHString } from '../libs/ETHString.sol';
 
 contract NounsClientTokenDescriptor is INounsClientTokenDescriptor {
     using Strings for uint256;
+    using ETHString for uint96;
 
     string private constant _SVG_START_TAG =
         '<svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="400" height="400" fill="#D5D7E1"/>';
     string private constant _SVG_END_TAG =
-        '<path d="M75 104H45V109H75V104Z" fill="#D19A54"/><path d="M110 104H80V109H110V104Z" fill="#D19A54"/><path d="M50 109H45V114H50V109Z" fill="#D19A54"/><path d="M60 109H50V114H60V109Z" fill="white"/><path d="M70 109H60V114H70V109Z" fill="black"/><path d="M75 109H70V114H75V109Z" fill="#D19A54"/><path d="M85 109H80V114H85V109Z" fill="#D19A54"/><path d="M95 109H85V114H95V109Z" fill="white"/><path d="M105 109H95V114H105V109Z" fill="black"/><path d="M110 109H105V114H110V109Z" fill="#D19A54"/><path d="M50 114H30V119H50V114Z" fill="#D19A54"/><path d="M60 114H50V119H60V114Z" fill="white"/><path d="M70 114H60V119H70V114Z" fill="black"/><path d="M85 114H70V119H85V114Z" fill="#D19A54"/><path d="M95 114H85V119H95V114Z" fill="white"/><path d="M105 114H95V119H105V114Z" fill="black"/><path d="M110 114H105V119H110V114Z" fill="#D19A54"/><path d="M35 119H30V124H35V119Z" fill="#D19A54"/><path d="M50 119H45V124H50V119Z" fill="#D19A54"/><path d="M60 119H50V124H60V119Z" fill="white"/><path d="M70 119H60V124H70V119Z" fill="black"/><path d="M75 119H70V124H75V119Z" fill="#D19A54"/><path d="M85 119H80V124H85V119Z" fill="#D19A54"/><path d="M95 119H85V124H95V119Z" fill="white"/><path d="M105 119H95V124H105V119Z" fill="black"/><path d="M110 119H105V124H110V119Z" fill="#D19A54"/><path d="M35 124H30V129H35V124Z" fill="#D19A54"/><path d="M50 124H45V129H50V124Z" fill="#D19A54"/><path d="M60 124H50V129H60V124Z" fill="white"/><path d="M70 124H60V129H70V124Z" fill="black"/><path d="M75 124H70V129H75V124Z" fill="#D19A54"/><path d="M85 124H80V129H85V124Z" fill="#D19A54"/><path d="M95 124H85V129H95V124Z" fill="white"/><path d="M105 124H95V129H105V124Z" fill="black"/><path d="M110 124H105V129H110V124Z" fill="#D19A54"/><path d="M75 129H45V134H75V129Z" fill="#D19A54"/><path d="M110 129H80V134H110V129Z" fill="#D19A54"/><line x1="30" y1="201.5" x2="370" y2="201.5" stroke="black" stroke-opacity="0.5"/></svg>';
+        '<path d="M75 340H45V345H75V340Z" fill="#D19A54"/><path d="M110 340H80V345H110V340Z" fill="#D19A54"/><path d="M50 345H45V350H50V345Z" fill="#D19A54"/><path d="M60 345H50V350H60V345Z" fill="white"/><path d="M70 345H60V350H70V345Z" fill="black"/><path d="M75 345H70V350H75V345Z" fill="#D19A54"/><path d="M85 345H80V350H85V345Z" fill="#D19A54"/><path d="M95 345H85V350H95V345Z" fill="white"/><path d="M105 345H95V350H105V345Z" fill="black"/><path d="M110 345H105V350H110V345Z" fill="#D19A54"/><path d="M50 350H30V355H50V350Z" fill="#D19A54"/><path d="M60 350H50V355H60V350Z" fill="white"/><path d="M70 350H60V355H70V350Z" fill="black"/><path d="M85 350H70V355H85V350Z" fill="#D19A54"/><path d="M95 350H85V355H95V350Z" fill="white"/><path d="M105 350H95V355H105V350Z" fill="black"/><path d="M110 350H105V355H110V350Z" fill="#D19A54"/><path d="M35 355H30V360H35V355Z" fill="#D19A54"/><path d="M50 355H45V360H50V355Z" fill="#D19A54"/><path d="M60 355H50V360H60V355Z" fill="white"/><path d="M70 355H60V360H70V355Z" fill="black"/><path d="M75 355H70V360H75V355Z" fill="#D19A54"/><path d="M85 355H80V360H85V355Z" fill="#D19A54"/><path d="M95 355H85V360H95V355Z" fill="white"/><path d="M105 355H95V360H105V355Z" fill="black"/><path d="M110 355H105V360H110V355Z" fill="#D19A54"/><path d="M35 360H30V365H35V360Z" fill="#D19A54"/><path d="M50 360H45V365H50V360Z" fill="#D19A54"/><path d="M60 360H50V365H60V360Z" fill="white"/><path d="M70 360H60V365H70V360Z" fill="black"/><path d="M75 360H70V365H75V360Z" fill="#D19A54"/><path d="M85 360H80V365H85V360Z" fill="#D19A54"/><path d="M95 360H85V365H95V360Z" fill="white"/><path d="M105 360H95V365H105V360Z" fill="black"/><path d="M110 360H105V365H110V360Z" fill="#D19A54"/><path d="M75 365H45V370H75V365Z" fill="#D19A54"/><path d="M110 365H80V370H110V365Z" fill="#D19A54"/><line x1="30" y1="314.5" x2="370" y2="314.5" stroke="black" stroke-opacity="0.5"/></svg>';
 
-    function tokenURI(
-        uint256 tokenId,
-        INounsClientTokenTypes.ClientMetadata memory metadata
-    ) external pure returns (string memory) {
+    function tokenURI(uint256 tokenId, INounsClientTokenTypes.ClientMetadata memory metadata)
+        external
+        pure
+        returns (string memory)
+    {
         string memory name = string(abi.encodePacked('Nouns Client ', tokenId.toString(), ': ', metadata.name));
         string memory image = Base64.encode(
             bytes(abi.encodePacked(_SVG_START_TAG, _generateSVGText(tokenId, metadata), _SVG_END_TAG))
@@ -61,24 +64,32 @@ contract NounsClientTokenDescriptor is INounsClientTokenDescriptor {
             );
     }
 
-    function _generateSVGText(
-        uint256 tokenId,
-        INounsClientTokenTypes.ClientMetadata memory metadata
-    ) private pure returns (string memory) {
+    function _generateSVGText(uint256 tokenId, INounsClientTokenTypes.ClientMetadata memory metadata)
+        private
+        pure
+        returns (string memory)
+    {
         return
             string(
                 abi.encodePacked(
-                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="20" font-weight="bold" letter-spacing="0em"><tspan x="30" y="165.822">VERIFIED NOUNS CLIENT</tspan></text>',
-                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="346.492">Client URL:</tspan></text>',
-                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="365.492">',
+                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="267.433">Client URL:</tspan></text>',
+                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="286.433">',
                     metadata.description,
                     '</tspan></text>',
-                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="295.492">Client ID:</tspan></text>',
-                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="314.492">',
+                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="211.433">Client ID:</tspan></text>',
+                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="230.433">',
                     tokenId.toString(),
                     '</tspan></text>',
-                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="244.492">Client Name:</tspan></text>',
-                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier New" font-size="15" font-weight="bold" letter-spacing="0em"><tspan x="30" y="263.492">',
+                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="155.433">Total rewarded:</tspan></text>',
+                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="174.433">',
+                    metadata.rewarded.toETHString(),
+                    ' ETH</tspan></text>',
+                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="99.4329">Status:</tspan></text>',
+                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="118.433">',
+                    metadata.approved ? 'Approved' : 'Not Approved',
+                    '</tspan></text>'
+                    '<text fill="black" fill-opacity="0.5" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="43.4329">Client Name:</tspan></text>',
+                    '<text fill="black" xml:space="preserve" style="white-space: pre" font-family="Courier" font-size="15" letter-spacing="0em"><tspan x="30" y="62.4329">',
                     metadata.name,
                     '</tspan></text>'
                 )
