d003fe857d65dc76c8a60ef6c0d57ad4aae896f3
davidbrai <davidbrai@gmail.com>
2024-04-08 12:14:14 +0200

    add test

--- packages/nouns-contracts/test/foundry/NounsDAOLogic/Propose.t.sol
+++ packages/nouns-contracts/test/foundry/NounsDAOLogic/Propose.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.15;
 
 import 'forge-std/Test.sol';
 import { NounsDAOLogicBaseTest } from './NounsDAOLogicBaseTest.sol';
+import { NounsDAOTypes } from '../../../contracts/governance/NounsDAOInterfaces.sol';
 
 contract ProposeTest is NounsDAOLogicBaseTest {
     address proposer = makeAddr('proposer');
@@ -58,5 +59,106 @@ contract ProposeTest is NounsDAOLogicBaseTest {
         vm.prank(proposer);
 
         dao.propose(targets, values, signatures, calldatas, 'some description');
+    }
+}
+
+contract ProposalDataForRewardsTest is NounsDAOLogicBaseTest {
+    address proposer = makeAddr('proposer');
+    address voter = makeAddr('voter');
+
+    function setUp() public override {
+        super.setUp();
+
+        vm.prank(address(dao.timelock()));
+        dao._setProposalThresholdBPS(1_000);
+
+        for (uint256 i = 0; i < 10; i++) {
+            mintTo(proposer);
+        }
+
+        for (uint256 i = 0; i < 3; i++) {
+            mintTo(voter);
+        }
+    }
+
+    function test_returnsData() public {
+        uint256 proposalId = propose(proposer, address(1), 0, '', '', 'proposal', 123);
+        uint32[] memory emptyArray = new uint32[](0);
+
+        NounsDAOTypes.ProposalForRewards[] memory data = dao.proposalDataForRewards({
+            firstProposalId: proposalId,
+            lastProposalId: proposalId,
+            proposalEligibilityQuorumBps: 0,
+            excludeCanceled: false,
+            votingClientIds: emptyArray
+        });
+
+        assertEq(data[0].clientId, 123);
+        assertEq(data[0].creationTimestamp, block.timestamp);
+    }
+
+    function test_excludesCanceledProposalsIfFlagIsOn() public {
+        uint256 proposalId = propose(proposer, address(1), 0, '', '', 'proposal', 123);
+
+        vm.prank(proposer);
+        dao.cancel(proposalId);
+
+        uint32[] memory emptyArray = new uint32[](0);
+        NounsDAOTypes.ProposalForRewards[] memory data = dao.proposalDataForRewards({
+            firstProposalId: proposalId,
+            lastProposalId: proposalId,
+            proposalEligibilityQuorumBps: 0,
+            excludeCanceled: true,
+            votingClientIds: emptyArray
+        });
+
+        assertEq(data.length, 0);
+    }
+
+    function test_includesCanceledProposalsIfFlagIsOff() public {
+        uint256 proposalId = propose(proposer, address(1), 0, '', '', 'proposal', 123);
+
+        vm.prank(proposer);
+        dao.cancel(proposalId);
+
+        uint32[] memory emptyArray = new uint32[](0);
+        NounsDAOTypes.ProposalForRewards[] memory data = dao.proposalDataForRewards({
+            firstProposalId: proposalId,
+            lastProposalId: proposalId,
+            proposalEligibilityQuorumBps: 0,
+            excludeCanceled: false,
+            votingClientIds: emptyArray
+        });
+
+        assertEq(data.length, 1);
+    }
+
+    function test_filtersProposalsBasedOnQuorum() public {
+        uint256 proposalId = propose(proposer, address(1), 0, '', '', 'proposal', 123);
+
+        uint32[] memory emptyArray = new uint32[](0);
+        NounsDAOTypes.ProposalForRewards[] memory data = dao.proposalDataForRewards({
+            firstProposalId: proposalId,
+            lastProposalId: proposalId,
+            proposalEligibilityQuorumBps: 2000,
+            excludeCanceled: false,
+            votingClientIds: emptyArray
+        });
+
+        assertEq(data.length, 0);
+
+        vm.roll(dao.proposals(proposalId).startBlock + 1);
+        vm.prank(voter);
+        dao.castRefundableVote(proposalId, 1);
+
+        data = dao.proposalDataForRewards({
+            firstProposalId: proposalId,
+            lastProposalId: proposalId,
+            proposalEligibilityQuorumBps: 2000,
+            excludeCanceled: false,
+            votingClientIds: emptyArray
+        });
+
+        assertEq(data.length, 1);
     }
 }
