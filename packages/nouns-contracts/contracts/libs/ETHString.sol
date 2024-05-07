88eda52108f509671cbda3c4573cae5684b59346
davidbrai <davidbrai@gmail.com>
2024-05-07 09:55:52 +0000

    make ETHString lib internal

--- packages/nouns-contracts/contracts/libs/ETHString.sol
+++ packages/nouns-contracts/contracts/libs/ETHString.sol
@@ -20,9 +20,9 @@ import { Strings } from '@openzeppelin/contracts/utils/Strings.sol';
 library ETHString {
     using Strings for uint256;
 
-    function toETHString(uint256 amount) public pure returns (string memory result) {
+    function toETHString(uint256 amount) internal pure returns (string memory result) {
         string memory integerPart = (amount / 1 ether).toString();
-        uint256 decimalPart = (amount % 1 ether) / 10**16; // 10^16 wei = 0.01 ETH
+        uint256 decimalPart = (amount % 1 ether) / 10 ** 16; // 10^16 wei = 0.01 ETH
 
         if (decimalPart == 0) {
             result = string(abi.encodePacked(integerPart, '.00'));
