872df04ccf325656cf0bf183a8e5693a81cc06f3
Milad Nekofar <milad@nekofar.com>
2025-04-27 18:06:10 +0300

    chore(deps-dev): update eslint config for Babel and TypeScript
    align with project needs by adding support for Babel parsing
    and TypeScript-specific configurations to improve linting.

--- packages/nouns-webapp/.eslintrc.js
+++ packages/nouns-webapp/.eslintrc.js
@@ -2,5 +2,24 @@ module.exports = {
   extends: [
     'react-app',
     'react-app/jest'
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react']
+    },
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
   ]
 };
