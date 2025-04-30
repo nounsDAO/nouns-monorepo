ba9d113ee4af49deee5dc9945601f86444d10430
Milad Nekofar <milad@nekofar.com>
2025-04-30 21:29:42 +0300

    feat(assets): add `tsup` config for bundling
    configured `tsup` for standardized builds and output formats.

---
+++ packages/nouns-assets/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.js',
+    };
+  },
+  target: 'es2020',
+  sourcemap: true,
+  treeshake: true,
+  loader: {
+    '.json': 'json',
+  },
+});
