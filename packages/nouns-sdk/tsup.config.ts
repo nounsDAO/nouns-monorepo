995ff3c834fa956f149f67c251780172b772ba5e
Milad Nekofar <milad@nekofar.com>
2025-04-30 20:26:34 +0300

    build(sdk): add `tsup` configuration for bundling
    set up `tsup` to streamline build process and support modern JS. This enables output in both ESM and CJS formats with source maps.

---
+++ packages/nouns-sdk/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.js',
+    };
+  },
+  target: 'es2020',
+  sourcemap: true,
+  loader: {
+    '.json': 'json',
+  },
+});
