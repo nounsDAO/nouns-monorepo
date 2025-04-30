6a1c918ac3f7e16321c99c6cd47187ee872bf4dd
Milad Nekofar <milad@nekofar.com>
2025-05-01 00:13:12 +0300

    feat(webapp): add lingui babel plugin in react config
    enhance localization support by enabling `@lingui/babel-plugin-lingui-macro` in `react` configuration.

--- packages/nouns-webapp/vite.config.ts
+++ packages/nouns-webapp/vite.config.ts
@@ -5,7 +5,14 @@ import { lingui } from '@lingui/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), lingui()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@lingui/babel-plugin-lingui-macro'],
+      },
+    }),
+    lingui(),
+  ],
   server: {
     port: 3000,
   },
