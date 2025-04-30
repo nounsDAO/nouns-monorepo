a95af69450724f64239de3d65b3615f7fd547166
Milad Nekofar <milad@nekofar.com>
2025-04-30 23:43:44 +0300

    chore(webapp): add `@lingui/vite-plugin` to plugins
    integrate `@lingui/vite-plugin` to enhance i18n support.

--- packages/nouns-webapp/vite.config.ts
+++ packages/nouns-webapp/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import { lingui } from '@lingui/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), lingui()],
   server: {
     port: 3000,
   },
