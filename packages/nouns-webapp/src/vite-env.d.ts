66c0a021061b4c20efbd98776c79855dcfb49f29
Milad Nekofar <milad@nekofar.com>
2025-05-01 16:21:08 +0300

    feat(webapp): add module declaration for `.svg` files
    enable TypeScript support for importing `.svg` files as React components. This improves type safety and enhances developer experience.

--- packages/nouns-webapp/src/vite-env.d.ts
+++ packages/nouns-webapp/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const SVG: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default SVG;
+}
