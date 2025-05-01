deb3859b14214a8316c8f9d2785af5dce3f5c57c
Milad Nekofar <milad@nekofar.com>
2025-05-01 23:20:17 +0300

    refactor(webapp): update SVG module declarations
    simplify and clean up `*.svg` module declarations to improve clarity and maintainability.

--- packages/nouns-webapp/src/vite-env.d.ts
+++ packages/nouns-webapp/src/vite-env.d.ts
@@ -1,18 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
-// plain .svg
-declare module '*.svg' {
-  import * as React from 'react'
-  const Component: React.FC<React.SVGProps<SVGSVGElement> & { title?: string }>
-  export default Component
-}
-
-// any query param after .svg (e.g. ?component or ?react or ?url)
-declare module '*.svg?*' {
-  import * as React from 'react'
-  const Component: React.FC<React.SVGProps<SVGSVGElement> & { title?: string }>
-  export default Component
+declare module '*.svg?react' {
+  import React from 'react';
+  const SVGComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default SVGComponent;
 }
 
 declare module 'bs-custom-file-input';
