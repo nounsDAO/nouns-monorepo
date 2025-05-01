d8646b966fcad61028044e421e696133ec013a84
Milad Nekofar <milad@nekofar.com>
2025-05-01 20:40:15 +0300

    chore(webapp): extract and relocate `lingui` configuration
    moved `lingui` configuration to a separate file for clarity and maintainability. This change improves organization and simplifies the `package.json`.

---
+++ packages/nouns-webapp/lingui.config.js
@@ -0,0 +1,23 @@
+export default {
+  catalogs: [
+    {
+      path: '<rootDir>/src/locales/{locale}',
+      include: ['<rootDir>/src'],
+    },
+  ],
+  compileNamespace: 'cjs',
+  fallbackLocales: {
+    default: 'en-US',
+    pseudo: 'en-US',
+  },
+  format: 'po',
+  formatOptions: {
+    lineNumbers: false,
+  },
+  locales: ['en-US', 'ja-JP', 'zh-CN', 'pseudo'],
+  orderBy: 'messageId',
+  rootDir: '.',
+  runtimeConfigModule: ['@lingui/core', 'i18n'],
+  sourceLocale: 'en-US',
+  pseudoLocale: 'pseudo',
+};
