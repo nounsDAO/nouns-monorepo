a19868f9ae562e1ea0899c9cb47d269ea3daf7dd
Milad Nekofar <milad@nekofar.com>
2025-05-04 19:40:48 +0300

    chore(lint-staged): migrate config to ES module
    migrated `lint-staged` config to ES module for consistency with modern JavaScript standards.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
   '*.{json,yaml,yml}': ['prettier --write'],
   '*.{md,mdx}': ['prettier --write'],
