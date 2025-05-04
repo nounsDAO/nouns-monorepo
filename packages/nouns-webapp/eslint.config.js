e6465df240e29bce4e0939dcb8731574b2b1f3ed
Milad Nekofar <milad@nekofar.com>
2025-05-04 20:58:57 +0300

    chore(webapp): comment out unused eslint plugins
    streamline ESLint configuration by removing unused plugins for `react` and `import`. This ensures the config remains minimal and easier to maintain.

--- packages/nouns-webapp/eslint.config.js
+++ packages/nouns-webapp/eslint.config.js
@@ -3,8 +3,8 @@ import globals from 'globals';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
-import importPlugin from 'eslint-plugin-import';
+// import reactPlugin from 'eslint-plugin-react';
+// import importPlugin from 'eslint-plugin-import';
 import unicornPlugin from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
@@ -13,8 +13,8 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      ...reactPlugin.configs.flat.recommended,
-      ...importPlugin.flatConfigs.recommended,
+      // ...reactPlugin.configs.flat.recommended,
+      // ...importPlugin.flatConfigs.recommended,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -24,8 +24,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooksPlugin,
       'react-refresh': reactRefreshPlugin,
-      react: reactPlugin,
-      import: importPlugin,
+      // react: reactPlugin,
+      // import: importPlugin,
       unicorn: unicornPlugin,
     },
     rules: {
