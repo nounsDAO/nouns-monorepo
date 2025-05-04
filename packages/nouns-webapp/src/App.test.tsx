2e91159f283709ea044bcab72f21931b6576512c
Milad Nekofar <milad@nekofar.com>
2025-05-04 03:58:22 +0300

    test(webapp): replace test with `test.todo`
    use `test.todo` to indicate unimplemented test logic for clarity.

--- packages/nouns-webapp/src/App.test.tsx
+++ packages/nouns-webapp/src/App.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test.todo('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   // Using a simpler assertion that doesn't require jest-dom
