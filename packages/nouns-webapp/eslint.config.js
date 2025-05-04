93371bda7ede26c2563e57f41b4be42623d39651
Milad Nekofar <milad@nekofar.com>
2025-05-04 04:11:29 +0300

    chore(webapp): update eslint config with react plugin
    add `eslint-plugin-react` for improved react-specific linting.

--- packages/nouns-webapp/eslint.config.js
+++ packages/nouns-webapp/eslint.config.js
@@ -3,11 +3,16 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...react.configs.flat.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
