3e01f3232add541178c6f5acbe52baf1777e6fc1
Milad Nekofar <milad@nekofar.com>
2025-05-22 03:12:01 +0300

    refactor(webapp): simplify Etherscan URL construction logic
    replaced custom URL logic with `defaultChain.blockExplorers.default`
    to improve maintainability and code clarity.

--- packages/nouns-webapp/src/utils/etherscan.ts
+++ packages/nouns-webapp/src/utils/etherscan.ts
@@ -1,59 +1,47 @@
-import { sepolia } from 'viem/chains';
-
 import { ETHERSCAN_API_KEY } from '@/config';
 import { defaultChain } from '@/wagmi';
 
-const getBaseURL = (network: number) => {
-  if (network === sepolia.id) {
-    return 'https://sepolia.etherscan.io/';
-  } else {
-    return 'https://etherscan.io/';
-  }
-};
-
-const BASE_URL = getBaseURL(defaultChain.id);
-
 export const buildEtherscanTxLink = (txHash: string): string => {
-  const path = `tx/${txHash}`;
-  return new URL(path, BASE_URL).toString();
+  const { url } = defaultChain.blockExplorers.default;
+  const path = `/tx/${txHash}`;
+
+  return new URL(path, url).toString();
 };
 
 export const buildEtherscanAddressLink = (address: string): string => {
-  const path = `address/${address}`;
-  return new URL(path, BASE_URL).toString();
+  const { url } = defaultChain.blockExplorers.default;
+  const path = `/address/${address}`;
+
+  return new URL(path, url).toString();
 };
 
 export const buildEtherscanTokenLink = (tokenContractAddress: string, tokenId: number): string => {
-  const path = `token/${tokenContractAddress}?a=${tokenId}`;
-  return new URL(path, BASE_URL).toString();
+  const { url } = defaultChain.blockExplorers.default;
+  const path = `/token/${tokenContractAddress}?a=${tokenId}`;
+
+  return new URL(path, url).toString();
 };
 
 export const buildEtherscanHoldingsLink = (address: string): string => {
-  const path = `tokenholdings?a=${address}`;
-  return new URL(path, BASE_URL).toString();
-};
+  const { url } = defaultChain.blockExplorers.default;
+  const path = `/tokenholdings?a=${address}`;
 
-const getApiBaseURL = (network: number) => {
-  if (network === sepolia.id) {
-    return 'https://api-sepolia.etherscan.io/';
-  } else {
-    return 'https://api.etherscan.io/';
-  }
+  return new URL(path, url).toString();
 };
-
-const API_BASE_URL = getApiBaseURL(defaultChain.id);
 
 export const buildEtherscanApiQuery = (
   address: string,
   module = 'contract',
   action = 'getsourcecode',
 ): string => {
+  const { apiUrl } = defaultChain.blockExplorers.default;
   const params = new URLSearchParams({
     module,
     action,
     address,
     apikey: ETHERSCAN_API_KEY,
   });
-  const path = `api?${params.toString()}`;
-  return new URL(path, API_BASE_URL).toString();
+  const path = `/?${params.toString()}`;
+
+  return new URL(path, apiUrl).toString();
 };
