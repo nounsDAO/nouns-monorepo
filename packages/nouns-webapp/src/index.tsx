e8aa5c546d03034dd7bd8b030ba4081bb95086a2
Volky <volkyeth@pm.me>
2025-05-26 16:31:40 -0300

    chore(webapp/theme): set up next-theme
    add next-theme to start managing light/dark themes in the app and eventually introduce a dark mode

--- packages/nouns-webapp/src/index.tsx
+++ packages/nouns-webapp/src/index.tsx
@@ -6,11 +6,12 @@ import './index.css';
 import { ApolloProvider, useQuery } from '@apollo/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import { parseAbiItem } from 'viem';
 import { hardhat } from 'viem/chains';
 import { usePublicClient, WagmiProvider } from 'wagmi';
 
+import { ThemeProvider } from '@/components/ThemeProvider';
 import { store } from '@/store';
 
 import App from './App';
@@ -212,21 +213,23 @@ const PastAuctions: React.FC = () => {
 };
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          <ChainSubscriber />
-          <ApolloProvider client={client}>
-            <PastAuctions />
-            <LanguageProvider>
-              <App />
-            </LanguageProvider>
-          </ApolloProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </React.StrictMode>
-  </Provider>,
+  <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light">
+    <ReduxProvider store={store}>
+      <React.StrictMode>
+        <WagmiProvider config={wagmiConfig}>
+          <QueryClientProvider client={queryClient}>
+            <ChainSubscriber />
+            <ApolloProvider client={client}>
+              <PastAuctions />
+              <LanguageProvider>
+                <App />
+              </LanguageProvider>
+            </ApolloProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </React.StrictMode>
+    </ReduxProvider>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
