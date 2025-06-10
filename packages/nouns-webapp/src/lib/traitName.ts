9e5f5075982e54036db444aa1304960dbccc9343
Volky <volkyeth@pm.me>
2025-06-09 21:43:35 -0300

    chore(webapp): add traitName and traitCategory utils

---
+++ packages/nouns-webapp/src/lib/traitName.ts
@@ -0,0 +1,24 @@
+import { ImageData } from '@noundry/nouns-assets';
+
+import { traitCategory } from '@/lib/traitCategory';
+import { INounSeed } from '@/wrappers/nounToken';
+
+const capitalizeFirstLetter = (s: string): string => s.charAt(0).toUpperCase() + s.slice(1);
+
+export const traitName = (type: keyof INounSeed, seed: number) => {
+  if (type === 'background') {
+    return ['Cool', 'Warm'][seed];
+  }
+
+  let filename = ImageData.images[traitCategory[type]][seed].filename;
+
+  if (type === 'glasses') {
+    filename = filename.replace('square-', '');
+  }
+
+  if (type === 'accessory') {
+    filename = filename.replace('body-', '');
+  }
+
+  return capitalizeFirstLetter(filename.substring(filename.indexOf('-') + 1).replace(/-/g, ' '));
+};
