f65b950dc3ff394c2a78a308e689800fcda85607
Volky <volkyeth@pm.me>
2025-06-11 09:46:09 -0300

    chore(webapp): remove netlify functions
    they aren't used by the webapp, and I checked the netlify logs for the last 30 days and there are 0 invocations, so nobody is using those

--- packages/nouns-webapp/functions/utils.ts
+++
@@ -1,9 +0,0 @@
-export const bigNumbersEqual = (a: string | number | bigint, b: string | number | bigint) => {
-  const aBigInt = typeof a === 'bigint' ? a : BigInt(a.toString());
-  const bBigInt = typeof b === 'bigint' ? b : BigInt(b.toString());
-  return aBigInt === bBigInt;
-};
-
-export const sharedResponseHeaders = {
-  'Access-Control-Allow-Origin': '*',
-};
