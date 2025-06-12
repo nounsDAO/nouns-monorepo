cc01d59253162d833cefca99ad13f19a4f3cb65d
Milad Nekofar <milad@nekofar.com>
2025-06-12 23:39:46 +0300

    refactor(webapp): simplify `Oracle` auction state handling
    streamline auction state checks and conditional rendering logic. This improves code readability and ensures clearer auction status display.

--- packages/nouns-webapp/src/pages/Oracle.tsx
+++ packages/nouns-webapp/src/pages/Oracle.tsx
@@ -160,6 +160,24 @@ export function OraclePage() {
     settleAuction({});
   }, [settleAuction]);
 
+  if (!(isNonNullish(auction) && auction.state === AuctionState.OverNotSettled)) {
+    return (
+      <div style={{ backgroundColor: stateBgColor }}>
+        <div className="container-xl">
+          <div className="flex w-full flex-col lg:flex-row">
+            <div className="mt-0 w-full max-w-full flex-shrink-0 pb-0 pl-3 pr-20 leading-6 text-neutral-800 lg:w-1/2 lg:flex-none">
+              <div>
+                <p>
+                  <Trans>Please wait for the auction to end before settling.</Trans>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div style={{ backgroundColor: stateBgColor }}>
       <div className="container-xl">
@@ -167,25 +185,21 @@ export function OraclePage() {
           <div className="mt-0 w-full max-w-full flex-shrink-0 px-3 lg:w-1/2 lg:flex-none">
             <div className="mx-[15%] w-[70%] self-end lg:mx-0 lg:w-auto">
               {(loading || isNullish(auction)) && <LoadingNoun />}
-              {!loading &&
-                isNonNullish(auction) &&
-                auction.state === AuctionState.OverNotSettled && (
-                  <Noun
-                    imgPath={nextNoun?.image ?? ''}
-                    alt={nextNoun?.nounId.toString() ?? t`Noun`}
-                  />
-                )}
+              {!loading && (
+                <Noun
+                  imgPath={nextNoun?.image ?? ''}
+                  alt={nextNoun?.nounId.toString() ?? t`Noun`}
+                />
+              )}
             </div>
           </div>
           <div className="mt-0 w-full max-w-full flex-shrink-0 pb-0 pl-3 pr-20 leading-6 text-neutral-800 lg:w-1/2 lg:flex-none">
-            {isNonNullish(auction) && auction.state === AuctionState.OverNotSettled && (
-              <AuctionControlPanel
-                nextNoun={nextNoun}
-                currentBlockTimestamp={currentBlockTimestamp}
-                onSettleAuction={handleSettleAuction}
-                isSettlingAuction={isSettlingAuction}
-              />
-            )}
+            <AuctionControlPanel
+              nextNoun={nextNoun}
+              currentBlockTimestamp={currentBlockTimestamp}
+              onSettleAuction={handleSettleAuction}
+              isSettlingAuction={isSettlingAuction}
+            />
           </div>
         </div>
       </div>
