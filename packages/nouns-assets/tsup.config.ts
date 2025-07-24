b84842515c295d7b0a60f9123743605aeb18cf77
Volky <volkyeth@pm.me>
2025-07-24 17:22:30 -0300

    build: fix root tsconfig references

--- packages/nouns-assets/tsup.config.ts
+++ packages/nouns-assets/tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      composite: false, // ref: https://github.com/egoist/tsup/issues/571#issuecomment-2457920686
+    },
+  },
   clean: true,
   outExtension({ format }) {
     return {
