a2d3867a04f90351d6d3c95219f87a4a1f2e817a
Volky <volkyeth@pm.me>
2025-08-06 15:12:40 -0300

    refactor(api): use abis from @nouns/sdk

--- packages/nouns-api/ponder.config.ts
+++ packages/nouns-api/ponder.config.ts
@@ -1,12 +1,12 @@
+import { nounsAuctionHouseAbi } from '@nouns/sdk/auction-house';
+import { nounsTokenAbi } from '@nouns/sdk/token';
+import { nounsGovernorAbi } from '@nouns/sdk/governor';
+import { nounsStreamFactoryAbi } from '@nouns/sdk/stream-factory';
+import { nounsStreamAbi } from '@nouns/sdk/stream';
 import { createConfig, factory } from 'ponder';
 import { getAbiItem } from 'viem';
 import dotenv from 'dotenv';
 
-import { NounsAuctionHouseV2Abi } from './abis/NounsAuctionHouseV2Abi';
-import { NounsDAOV4Abi } from './abis/NounsDAOV4Abi';
-import { NounsTokenAbi } from './abis/NounsTokenAbi';
-import { streamAbi } from './abis/StreamAbi';
-import { streamFactoryAbi } from './abis/StreamFactoryAbi';
 dotenv.config();
 
 export default createConfig({
@@ -17,25 +17,25 @@ export default createConfig({
     NounsAuctionHouseV2: {
       chain: 'mainnet',
       address: '0x830BD73E4184ceF73443C15111a1DF14e495C706',
-      abi: NounsAuctionHouseV2Abi,
+      abi: nounsAuctionHouseAbi,
       startBlock: 12985451,
     },
     NounsToken: {
       chain: 'mainnet',
       address: '0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03',
-      abi: NounsTokenAbi,
+      abi: nounsTokenAbi,
       startBlock: 12985438,
     },
     NounsDAOV4: {
       chain: 'mainnet',
       address: '0x6f3E6272A167e8AcCb32072d08E0957F9c79223d',
-      abi: NounsDAOV4Abi,
+      abi: nounsGovernorAbi,
       startBlock: 12985453,
     },
     StreamFactory: {
       chain: 'mainnet',
       address: '0x0fd206FC7A7dBcD5661157eDCb1FFDD0D02A61ff',
-      abi: streamFactoryAbi,
+      abi: nounsStreamFactoryAbi,
       startBlock: 16576500,
     },
 
@@ -43,10 +43,10 @@ export default createConfig({
       chain: 'mainnet',
       address: factory({
         address: '0x0fd206FC7A7dBcD5661157eDCb1FFDD0D02A61ff',
-        event: getAbiItem({ abi: streamFactoryAbi, name: 'StreamCreated' }),
-        parameter: getAbiItem({ abi: streamFactoryAbi, name: 'StreamCreated' }).inputs[7].name,
+        event: getAbiItem({ abi: nounsStreamFactoryAbi, name: 'StreamCreated' }),
+        parameter: getAbiItem({ abi: nounsStreamFactoryAbi, name: 'StreamCreated' }).inputs[7].name,
       }),
-      abi: streamAbi,
+      abi: nounsStreamAbi,
       startBlock: 16576500,
     },
     // NounsDAOData: {
