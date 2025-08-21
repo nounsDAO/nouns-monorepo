9426f5ac48f5bf55e2804738da8d8d6234a437c3
Milad Nekofar <milad@nekofar.com>
2025-08-21 13:16:14 +0300

    refactor(webapp): replace `Col` and `Image` with Tailwind
    Refactor layout to use Tailwind utility classes for styling. Improves consistency with the rest of the project and reduces dependency on `react-bootstrap`.

--- packages/nouns-webapp/src/components/not-found-page/index.tsx
+++ packages/nouns-webapp/src/components/not-found-page/index.tsx
@@ -1,5 +1,4 @@
 import { Trans } from '@lingui/react/macro';
-import { Col, Image } from 'react-bootstrap';
 
 import _404img from '@/assets/404noun.png';
 import Section from '@/components/section';
@@ -9,17 +8,20 @@ import classes from './not-found.module.css';
 const NotFoundPage = () => {
   return (
     <Section fullWidth={false}>
-      <Col lg={4}>
-        <Image
-          src={typeof _404img === 'string' ? _404img : (_404img as { src: string }).src}
-          fluid
-        />
-      </Col>
-      <Col lg={8}>
-        <h1 className={classes.heading}>
-          <Trans>404: This is not the person, place, or thing you&apos;re looking for...</Trans>
-        </h1>
-      </Col>
+      <div className="grid grid-cols-1 items-center gap-6 lg:grid-cols-12">
+        <div className="lg:col-span-4">
+          <img
+            src={typeof _404img === 'string' ? _404img : (_404img as { src: string }).src}
+            alt="404 Noun"
+            className="h-auto max-w-full"
+          />
+        </div>
+        <div className="lg:col-span-8">
+          <h1 className={classes.heading}>
+            <Trans>404: This is not the person, place, or thing you&apos;re looking for...</Trans>
+          </h1>
+        </div>
+      </div>
     </Section>
   );
 };
