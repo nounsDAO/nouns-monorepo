83563f2c152c82ac0e6ddacf9362492de0269f22
Milad Nekofar <milad@nekofar.com>
2025-08-24 23:57:26 +0300

    style(webapp): replace CSS module in brand-spinner with Tailwind
    Removes `brand-spinner.module.css` in favor of Tailwind classes. Simplifies styles, improves maintainability, and aligns with project-wide conventions.

--- packages/nouns-webapp/src/components/brand-spinner/index.tsx
+++ packages/nouns-webapp/src/components/brand-spinner/index.tsx
@@ -1,10 +1,8 @@
-import classes from './brand-spinner.module.css';
-
 const BrandSpinner = () => {
   return (
     <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        className={classes.spinner}
+        className="animate-[spin_1.25s_linear_infinite]; !origin-center"
         d="M23 13C23 14.9778 22.3816 16.9112 21.223 18.5557C20.0644 20.2002 18.4176 21.4819 16.4909 22.2388C14.5642 22.9957 12.4441 23.1937 10.3988 22.8078C8.35342 22.422 6.47463 21.4696 5 20.0711"
         stroke="black"
         strokeWidth="4"
