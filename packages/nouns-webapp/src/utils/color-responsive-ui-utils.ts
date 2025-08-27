fbc43396e7b0d528c7b5c7c542985f1897c484f1
Milad Nekofar <milad@nekofar.com>
2025-08-27 22:25:07 +0300

    refactor(webapp): extract and reuse `useShouldUseStateBg`
    introduced `useShouldUseStateBg` hook for clarity and reuse. This simplifies logic, improves readability, and reduces redundancy in state checks.

--- packages/nouns-webapp/src/utils/color-responsive-ui-utils.ts
+++ packages/nouns-webapp/src/utils/color-responsive-ui-utils.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { usePathname } from 'next/navigation';
 
 import { useAppSelector } from '@/hooks';
@@ -18,16 +20,20 @@ export const shouldUseStateBg = (location: { pathname: string }) => {
  * @param warmState  What to return is the state is warm
  * @returns item corresponding to current state
  */
-export const usePickByState = <T>(whiteState: T, coolState: T, warmState: T): T => {
+export const useShouldUseStateBg = (): boolean => {
   const pathname = usePathname();
-  const useStateBg = shouldUseStateBg({ pathname });
+  return shouldUseStateBg({ pathname });
+};
+
+/**
+ * Utility hook that takes three items and returns the one corresponding to the current page state.
+ */
+export const usePickByState = <T>(whiteState: T, coolState: T, warmState: T): T => {
+  const useStateBg = useShouldUseStateBg();
   const isCoolState = useAppSelector(state => state.application.isCoolBackground);
 
   if (!useStateBg) {
     return whiteState;
   }
-  if (isCoolState) {
-    return coolState;
-  }
-  return warmState;
+  return isCoolState ? coolState : warmState;
 };
