cbbc6f0de71955ddc08dd5c5da602437caf5ded7
Drew Modrov <1103446+modrovsky@users.noreply.github.com>
2026-04-24 13:50:12 -0400

    remove unused nouns-docs package
    Remove unused nouns-docs package that was breaking CI.

--- packages/nouns-docs/app/_meta.global.tsx
+++
@@ -1,7 +0,0 @@
-export default {
-  nouns_wtf: {
-    type: 'page',
-    title: 'Nouns.wtf',
-    href: 'https://nouns.wtf',
-  },
-};
--- packages/nouns-docs/postcss.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
